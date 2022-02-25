8000d16f940c4602b3d2369e1d5707698448e1ae
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2022-02-26 05:13:17 +0530

    Create blaBla.py

---
+++ plugins/dm/callBack/blaBla.py
@@ -0,0 +1,67 @@
+# fileName : plugins/dm/callBack/blaBla.py
+# copyright ©️ 2021 nabilanavab
+
+
+
+
+from pdf import PROCESS
+from pyrogram import filters
+from pyrogram import Client as ILovePDF
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+
+
+#--------------->
+#--------> LOCAL VARIABLES
+#------------------->
+
+error = filters.create(lambda _, __, query: query.data == "error")
+closeme = filters.create(lambda _, __, query: query.data == "closeme")
+underDev = filters.create(lambda _, __, query: query.data == "underDev")
+notEncrypted = filters.create(lambda _, __, query: query.data == "notEncrypted")
+
+
+@ILovePDF.on_callback_query(underDev)
+async def _underDev(bot, callbackQuery):
+    try:
+        await callbackQuery.answer(
+            "This feature is Under Development ⛷️"
+        )
+    except Exception:
+        pass
+
+
+@ILovePDF.on_callback_query(error)
+async def _error(bot, callbackQuery):
+    try:
+        await callbackQuery.answer(
+            "Error annenn paranjille.. then what.. 😏"
+        )
+    except Exception:
+        pass
+
+
+@ILovePDF.on_callback_query(closeme)
+async def _closeme(bot, callbackQuery):
+    try:
+        try:
+            await callbackQuery.message.delete()
+        except Exception:
+            pass
+        await callbackQuery.answer(
+            "Process Canceled.. 😏"
+        )
+        PROCESS.remove(callbackQuery.message.chat.id)
+    except Exception:
+        pass
+
+
+@ILovePDF.on_callback_query(notEncrypted)
+async def _notEncrypted(bot, callbackQuery):
+    try:
+        await callbackQuery.answer(
+            "File Not Encrypted.. 👀"
+        )
+    except Exception:
+        pass
