41d6729c67dbe538d23696addca754d64d42427b
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2022-02-26 05:18:30 +0530

    Create cancel.py

---
+++ plugins/dm/cancel.py
@@ -0,0 +1,73 @@
+# fileName : plugins/dm/cancel.py
+# copyright ©️ 2021 nabilanavab
+
+
+
+
+from pdf import PROCESS
+from pyrogram import filters
+from Configs.dm import Config
+from pyrogram import Client as ILovePDF
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup
+
+
+
+
+#--------------->
+#--------> config vars
+#------------------->
+
+BANNED_USERS=Config.BANNED_USERS
+ADMIN_ONLY=Config.ADMIN_ONLY
+ADMINS=Config.ADMINS
+
+#--------------->
+#--------> LOCAL VARIABLES
+#------------------->
+
+UCantUse = "🚫For Some Reason You Can't Use This Bot🚫"
+
+button=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "😉 Create your Own 😉",
+                    url="https://github.com/nabilanavab/ilovepdf"
+                )
+            ]
+       ]
+    )
+
+#--------------->
+#--------> CANCELS CURRENT PDF TO IMAGES WORK
+#------------------->
+
+
+@ILovePDF.on_message(filters.private & ~filters.edited & filters.command(["cancel"]))
+async def cancelP2I(bot, message):
+    try:
+        if (message.chat.id in BANNED_USERS) or (
+            (ADMIN_ONLY) and (message.chat.id not in ADMINS)
+        ):
+            await message.reply_text(
+                UCantUse,
+                reply_markup=button,
+                quote=True
+            )
+            return
+        PROCESS.remove(message.chat.id)
+        await message.delete()          # delete /cancel if process canceled
+    except Exception:
+        try:
+            await bot.send_chat_action(
+                message.chat.id, "typing"
+            )
+            await message.reply_text(
+                '🤔', quote=True
+            )
+        except Exception:
+            pass
+
+
+#                                                                                  Telegram: @nabilanavab
