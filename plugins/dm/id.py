cbf78cb41bffc8993f1b29b4770396f712a0f932
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2022-02-26 05:30:56 +0530

    Create id.py

---
+++ plugins/dm/id.py
@@ -0,0 +1,68 @@
+# fileName : plugins/dm/id.py
+# copyright ©️ 2021 nabilanavab
+
+
+
+
+from pyrogram import filters
+from Configs.dm import Config
+from pyrogram import Client as ILovePDF
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup
+
+
+
+
+#--------------->
+#--------> Config var.
+#------------------->
+
+BANNED_USERS=Config.BANNED_USERS
+ADMIN_ONLY=Config.ADMIN_ONLY
+ADMINS=Config.ADMINS
+
+#--------------->
+#--------> LOCAL VARIABLES
+#------------------->
+
+UCantUse = "For Some Reason You Can't Use This Bot 🛑"
+
+
+button=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "😉 Create your Own 😉",
+                    url="https://github.com/nabilanavab/ilovepdf"
+                )
+            ]
+       ]
+    )
+
+#--------------->
+#--------> GET USER ID (/id)
+#------------------->
+
+
+@ILovePDF.on_message(filters.private & ~filters.edited & filters.command(["id"]))
+async def userId(bot, message):
+    try:
+        await bot.send_chat_action(
+            message.chat.id, "typing"
+        )
+        if (message.chat.id in BANNED_USERS) or (
+            (ADMIN_ONLY) and (message.chat.id not in ADMINS)
+        ):
+            await message.reply_text(
+                UCantUse,
+                reply_markup=button
+            )
+            return
+        await message.reply_text(
+            f'Your Id: `{message.chat.id}`', quote=True
+        )
+    except Exception:
+        pass
+
+
+#                                                                                  Telegram: @nabilanavab
