4f123bd6bb87f5d2b88fa09f137ce51643fd6b57
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2022-03-15 06:49:21 +0530

    Update start.py

--- plugins/dm/start.py
+++ plugins/dm/start.py
@@ -1,19 +1,12 @@
 # fileName : plugins/dm/start.py
 # copyright ©️ 2021 nabilanavab
 
-
-
-
 from pdf import invite_link
 from pyrogram import filters
 from Configs.dm import Config
 from pyrogram import Client as ILovePDF
 from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
-
-
-
-
 
 #--------------->
 #--------> LOCAL VARIABLES
@@ -32,9 +25,7 @@ Update Channel: @ilovepdf_bot 💎
 [Source Code 🏆](https://github.com/nabilanavab/ilovepdf)
 [Write a feedback 📋](https://t.me/nabilanavabchannel/17?comment=10)"""
 
-
 UCantUse = "For Some Reason You Can't Use This Bot 🛑"
-
 
 forceSubMsg = """Wait [{}](tg://user?id={})..!!
 
@@ -44,7 +35,6 @@ This Means You Need To Join The Below Mentioned Channel for Using Me!
 
 hit on "retry ♻️" after joining.. 😅"""
 
-
 aboutDev = """Owned By: @nabilanavab
 Update Channel: @ilovepdf_bot
 
@@ -53,13 +43,11 @@ Now its easy to create your Own nabilanavab/ilovepdf bot
 [Source Code 🏆](https://github.com/nabilanavab/ilovepdf)
 [Write a feedback 📋](https://t.me/nabilanavabchannel/17?comment=10)"""
 
-
 exploreBotEdit = """
 [WORKING IN PROGRESS
 
 Join @ilovepdf_bot bot Updates 💎](https://t.me/ilovepdf_bot)
 """
-
 
 foolRefresh = "വിളച്ചിലെടുക്കല്ലേ കേട്ടോ 😐"
 
@@ -76,19 +64,16 @@ ADMINS=Config.ADMINS
 #--------> /start (START MESSAGE)
 #------------------->
 
-
 @ILovePDF.on_message(filters.private & ~filters.edited & filters.command(["start"]))
 async def start(bot, message):
         global invite_link
-        await bot.send_chat_action(
-            message.chat.id, "typing"
-        )
+        await message.reply_chat_action("typing")
         # CHECK IF USER BANNED, ADMIN ONLY..
         if (message.chat.id in BANNED_USERS) or (
             (ADMIN_ONLY) and (message.chat.id not in ADMINS)
         ):
-            await bot.send_message(
-                message.chat.id, UCantUse
+            await message.reply_text(
+                UCantUse, quote=True
             )
             return
         # CHECK USER IN CHANNEL (IF UPDATE_CHANNEL ADDED)
@@ -102,8 +87,7 @@ async def start(bot, message):
                     invite_link = await bot.create_chat_invite_link(
                         int(UPDATE_CHANNEL)
                     )
-                await bot.send_message(
-                    message.chat.id,
+                await message.reply_text(
                     forceSubMsg.format(
                         message.from_user.first_name, message.chat.id
                     ),
@@ -124,59 +108,48 @@ async def start(bot, message):
                         ]
                     )
                 )
-                await bot.delete_messages(
-                    chat_id = message.chat.id,
-                    message_ids = message.message_id
-                )
+                await message.delete()
                 return
         
-        await bot.send_message(
-            message.chat.id,
+        await message.reply_text(
             welcomeMsg.format(
                 message.from_user.first_name,
                 message.chat.id
             ),
-            disable_web_page_preview = True,
+            disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton(
                             "🌟 Source Code 🌟",
-                            callback_data = "strtDevEdt"
+                            callback_data="strtDevEdt"
                         ),
                         InlineKeyboardButton(
                             "Explore Bot 🎊",
-                            callback_data = "exploreBot"
+                            callback_data="exploreBot"
                         )
                     ],
                     [
                         InlineKeyboardButton(
                             "Close 🚶",
-                            callback_data = "close"
+                            callback_data="close"
                         )
                     ]
                 ]
             )
         )
         # DELETES /start MESSAGE
-        await bot.delete_messages(
-            chat_id = message.chat.id,
-            message_ids = message.message_id
-        )
-
+        await message.delete()
 
 #--------------->
 #--------> START CALLBACKS
 #------------------->
-
 
 strtDevEdt = filters.create(lambda _, __, query: query.data == "strtDevEdt")
 exploreBot = filters.create(lambda _, __, query: query.data == "exploreBot")
 refresh = filters.create(lambda _, __, query: query.data == "refresh")
 close = filters.create(lambda _, __, query: query.data == "close")
 back = filters.create(lambda _, __, query: query.data == "back")
-
-
 
 @ILovePDF.on_callback_query(strtDevEdt)
 async def _strtDevEdt(bot, callbackQuery):
@@ -208,7 +181,6 @@ async def _strtDevEdt(bot, callbackQuery):
     except Exception as e:
         print(e)
 
-
 @ILovePDF.on_callback_query(exploreBot)
 async def _exploreBot(bot, callbackQuery):
     try:
@@ -234,7 +206,6 @@ async def _exploreBot(bot, callbackQuery):
         return
     except Exception as e:
         print(e)
-
 
 @ILovePDF.on_callback_query(back)
 async def _back(bot, callbackQuery):
@@ -269,7 +240,6 @@ async def _back(bot, callbackQuery):
         return
     except Exception as e:
         print(e)
-
 
 @ILovePDF.on_callback_query(refresh)
 async def _refresh(bot, callbackQuery):
@@ -319,7 +289,6 @@ async def _refresh(bot, callbackQuery):
         except Exception as e:
             print(e)
 
-
 @ILovePDF.on_callback_query(close)
 async def _close(bot, callbackQuery):
     try:
@@ -330,6 +299,5 @@ async def _close(bot, callbackQuery):
         return
     except Exception as e:
         print(e)
-
 
 #                                                                                  Telegram: @nabilanavab
