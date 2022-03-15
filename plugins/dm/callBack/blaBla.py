e5fcc47b613361b0a8e5c97ad071f2d62086dd39
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2022-03-15 06:53:13 +0530

    Update blaBla.py

--- plugins/dm/callBack/blaBla.py
+++ plugins/dm/callBack/blaBla.py
@@ -1,16 +1,10 @@
-# fileName : plugins/dm/callBack/blaBla.py
+# fileName : plugins/dm/callBack/underDev.py
 # copyright ©️ 2021 nabilanavab
-
-
-
 
 from pdf import PROCESS
 from pyrogram import filters
 from pyrogram import Client as ILovePDF
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
-
-
 
 #--------------->
 #--------> LOCAL VARIABLES
@@ -18,7 +12,11 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 error = filters.create(lambda _, __, query: query.data == "error")
 closeme = filters.create(lambda _, __, query: query.data == "closeme")
+closeALL = filters.create(lambda _, __, query: query.data == "closeALL")
 underDev = filters.create(lambda _, __, query: query.data == "underDev")
+canceled = filters.create(lambda _, __, query: query.data == "canceled")
+completed = filters.create(lambda _, __, query: query.data == "completed")
+cancelP2I = filters.create(lambda _, __, query: query.data == "cancelP2I")
 notEncrypted = filters.create(lambda _, __, query: query.data == "notEncrypted")
 
 
@@ -62,6 +60,50 @@ async def _notEncrypted(bot, callbackQuery):
     try:
         await callbackQuery.answer(
             "File Not Encrypted.. 👀"
+        )
+    except Exception:
+        pass
+
+
+@ILovePDF.on_callback_query(closeALL)
+async def _closeALL(bot, callbackQuery):
+    try:
+        await callbackQuery.message.delete()
+        await callbackQuery.message.reply_to_message.delete()
+    except Exception as e:
+        print(e)
+
+
+@ILovePDF.on_callback_query(cancelP2I)
+async def _cancelP2I(bot, callbackQuery):
+    try:
+        await callbackQuery.message.edit_reply_markup(
+            InlineKeyboardMarkup(
+                [[
+                    InlineKeyboardButton("💤 CANCELING.. 💤", callback_data="n")
+                ]]
+            )
+        )
+        PROCESS.remove(callbackQuery.message.chat.id)
+    except Exception:
+        pass
+
+
+@ILovePDF.on_callback_query(canceled)
+async def _canceled(bot, callbackQuery):
+    try:
+        await callbackQuery.answer(
+            "Nothing Official About it.. 😅"
+        )
+    except Exception:
+        pass
+
+
+@ILovePDF.on_callback_query(completed)
+async def _completed(bot, callbackQuery):
+    try:
+        await callbackQuery.answer(
+            "🎉 Completed.. 🏃"
         )
     except Exception:
         pass
