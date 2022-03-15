48a5d1a98b58c692d4c95fb1a77dfbbe9f77cdd6
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2022-03-15 07:08:18 +0530

    Update waste.py

--- plugins/dm/waste.py
+++ plugins/dm/waste.py
@@ -1,17 +1,11 @@
 # fileName : plugins/dm/waste.py
 # copyright ©️ 2021 nabilanavab
 
-
-
-
 from pyrogram import filters
 from Configs.dm import Config
 from pyrogram import Client as ILovePDF
 from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
-
-
-
 
 #--------------->
 #--------> Config var.
@@ -43,26 +37,21 @@ button=InlineKeyboardMarkup(
 #--------> PDF REPLY BUTTON
 #------------------->
 
-
 @ILovePDF.on_message(filters.private & ~filters.edited)
 async def spam(bot, message):
     try:
-        await bot.send_chat_action(
-            message.chat.id, "typing"
-        )
+        await message.reply_chat_action("typing")
         if (message.chat.id in BANNED_USERS) or (
             (ADMIN_ONLY) and (message.chat.id not in ADMINS)
         ):
             await message.reply_text(
-                UCantUse,
-                reply_markup=button
+                UCantUse, reply_markup=button
             )
             return
         await message.reply_text(
-            f"`unsupported media..😪`", quote=True
+            f"`Wdym` 🤔, __if you are looking for text to pdf try:__ /txt2pdf..😜", quote=True
         )
     except Exception:
         pass
-
 
 #                                                                                  Telegram: @nabilanavab
