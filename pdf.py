76bd484357e3e7d215a47f6b288994709bd7cb9f
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2022-03-27 23:31:14 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -2,7 +2,6 @@
 # -*- COADING: UTF-8 -*-
 # COPYRIGHT ©️ 2021 NABILANAVAB
 
-
 '''
   _   _                  ___  ___  ____ ™
  | | | |   _____ _____  | _ \|   \|  __| 
@@ -13,13 +12,10 @@
                          Telegram: @nabilanavab
  '''
 
-
-
 import logging
 from pyromod import listen
 from Configs.dm import Config
 from pyrogram import Client, idle
-
 
 # LOGGING INFO: DEBUG
 logging.basicConfig(
@@ -33,12 +29,10 @@ PDF = {}            # save images for generating pdf
 PROCESS = []        # to check current process
 invite_link = None
 
-
 # PLUGIN DIRECTORY
 plugin = dict(
     root = "plugins"
 )
-
 
 # PYROGRAM BOT AUTHENTIFICATION
 bot = Client(
@@ -49,7 +43,6 @@ bot = Client(
     api_hash = Config.API_HASH,
     bot_token = Config.API_TOKEN
 )
-
 
 bot.start()
 idle()
