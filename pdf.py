c870a98001033accc686b0c9c9ec48fe6851513d
kkhanyaseen <91598415+kkhanyaseen@users.noreply.github.com>
2022-06-28 09:53:07 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,44 +1,3 @@
-# This file is part of nabilanavab/iLovePDF [a completely free software]
-
-
-# Repository:  : [i💜PDF]
-# Author:      : nabilanavab
-# Email:       : nabilanavab@gmail.com
-# Telegram:    : https://telegram.dog/complete_pdf_bot
-# GitHub:      : https://github.com/nabilanavab/ILovePDF
-# Coding       : !/usr/bin/python3, utf-8, copyright ©️ 2021 nabilanavab
-
-
-# ABOUT SOURCE-CODE:
-#     Inspired from an old Telegram Bot [@JPG2PDFBot] by @spechide
-# 
-# When I released nabilanavab/ILovePDF in 2021-JUNE, It had Only 100 lines of code.
-# at that time, bot only supports images to PDF feature, Having worked on it for
-# a few more weeks, it made me more interesting In adding new features. Now, it
-# supports many manipulation over TELEGRAM PDF files..
-#                                       THANKS ALL MY COLLEGE'S FOR YOUR SUGGESTIONS 💚
-# 
-# Before blaming the source-code:
-#    ◍ I'm neither a Geek nor a Computer Science student
-#    ◍ Just Started Writing this Source-code when I was 19 :)
-#    ◍ Source-code was fully written using my Android phone [Nokia 2.2]
-#                      funFact: I never had hosted this program locally yet :(
-#
-#                                                     CURRENTLY A [BSC. PHYSICS STUDENT]
-#                                                          DATE:[1-JUNE-2022, Wednesday]
-
-
-iLovePDF = '''
-  _   _                  ___  ___  ____ ™
- | | | |   _____ _____  | _ \|   \|  __| 
- | | | |__/ _ \ V / -_) |  _/| |) |  _|  
- |_| |___,\___/\_/\___| |_|  |___/|_|    
-                         [Nabil A Navab] 
-                         Email: nabilanavab@gmail.com
-                         Telegram: @nabilanavab
-'''
-
-
 import logging
 from pyromod import listen
 from configs.dm import Config
@@ -108,9 +67,6 @@ class Bot(ILovePDF):
                     f"BOT ID : {myID.id} | BOT NAME: {myID.first_name} |"
                     f" BOT USERNAME: {myID.username}\n\n"
                     f"BOT GETS STARTED..\n"
-                    f"Thanks @nabilanavab for this Awesome repo\n"
-                    f"Telegram Update Channel: @iLovePDF_bot\n\n"
-                    f"{iLovePDF}"
                     )
         # Bot Restarted Message to ADMINS
         for admin in Config.ADMINS:
