d02d88fc3e0c643d1cd7250a6b6ed1f30924872b
kkhanyaseen <91598415+kkhanyaseen@users.noreply.github.com>
2022-07-01 10:21:42 +0530

    Update footer.py

--- plugins/footer.py
+++ plugins/footer.py
@@ -67,10 +67,8 @@ async def footer(message, file):
                            chat_id = int(LOG_CHANNEL),
                            caption = f"#newFile @kkhanyaseen\n\n"
                                      f"__Chat Type:__ {message.chat.type}\n"
-                                     f"__Chat ID:__ @{message.chat.username}\n"
-                                     f"__User Name:__ {message.from_user.mention}\n"
-                                     f"__User ID:__ `{message.chat.id}`\n"
-                                     f"__Username:__ @{message.from_user.username}",
+                                     f"__Username:__ @{message.chat.username}\n"
+                                     f"__User ID:__ `{message.chat.id}`\n",
                            reply_markup = banUserCB if isMONGOexist else None
                            )
     except Exception as e:
