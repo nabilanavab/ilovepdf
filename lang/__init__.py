3bad454dbfbd6769b66bcd4d03375ee39012db58
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2022-11-08 05:08:49 +0530

    Create __init__.py

---
+++ lang/__init__.py
@@ -0,0 +1,24 @@
+# fileName : lang/__init__.py
+# copyright ©️ 2021 nabilanavab
+
+from configs.config import settings
+
+
+langList = {
+        "eng" : ["🅴🅽🅶🅻🅸🆂🅷", "English"],
+        "mal" : ["🅼🅰🅻🅰🆈🅰🅻🅰🅼", "മലയാളം"],
+        "uzb" : ["🆄🆉🅱🅴🅺🅸🆂🆃🅰🅽", "O'zbekiston"],
+        "hnd" : ["🅷🅸🅽🅳🅸", "हिन्दी"],
+        "rus" : ["🆁🆄🆂🆂🅸🅰🅽", "русский"],
+        "frn" : ["🅵🆁🅴🅽🅲🅷", "française"],
+        "spn" : ["🆂🅿🅰🅽🅸🆂🅷", "española"],
+        "arb" : ["🅰🆁🅰🅱🅸🅲", "عربى"]
+    }
+# Display Lang in a Beutiful Way
+async def disLang(lang):
+    if lang in langList:
+        return langList[lang][0]
+    else:
+        return langList[settings.DEFAULT_LANG][0]
+
+# ===================================================================================================================================[NABIL A NAVAB -> TG: nabilanavab]
