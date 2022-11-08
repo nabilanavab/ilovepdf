cb9d27e791fccdd46e917f74137579bcd8d73fc7
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2022-11-09 00:20:51 +0530

    Update setLang.py

--- plugins/dm/callBack/inline_query/setLang.py
+++ plugins/dm/callBack/inline_query/setLang.py
@@ -25,21 +25,13 @@ async def setLang(bot, inline_query):
         BUTTON1.update(_lang); BUTTON1.update({"♻" : "-|refresh"})
         BUTTON1 = await createBUTTON(btn=BUTTON1, order=int(f"1{((len(BUTTON1)-2)//3)*'3'}{(len(BUTTON1)-2)%3}1"))
         
-        BUTTON2 = CHUNK['TOP']
-        __lang = { langList[lang][1]:f"https://t.me/{myID.username}?start=-l{lang}-r{inline_query.from_user.id}" for lang in langList }
-        BUTTON2.update(__lang)
-        BUTTON2 = await createBUTTON(btn=BUTTON2, order=int(f"1{((len(BUTTON2)-1)//3)*'3'}{(len(BUTTON2)-1)%3}"))
-        
         await inline_query.answer(
             results = [
                 InlineQueryResultPhoto(
-                    photo_url = images.THUMBNAIL_URL, title = "i ❤ PDF", reply_markup = BUTTON1,
+                    photo_url = images.THUMBNAIL_URL, reply_markup = BUTTON1,
+                    title = f"i ❤ PDF\n\nBot: {myID.username}\n"
+                            "Update Channel: @ilovepdf_bot",
                     input_message_content = InputTextMessageContent("set Language 🌐"),
-                    caption = CHUNK['capt'], description = CHUNK['des']
-                ),
-                InlineQueryResultPhoto(
-                    photo_url = images.THUMBNAIL_URL, title = "i ❤ PDF | referal", reply_markup = BUTTON2,
-                    input_message_content = InputTextMessageContent("set Language: 🌐"),
                     caption = CHUNK['capt'], description = CHUNK['des']
                 )
             ]
