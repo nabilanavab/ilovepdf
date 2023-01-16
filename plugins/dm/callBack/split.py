37cfccd81f556d1197d4bf41d5b9a659a6f0c03d
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-01-16 11:58:36 +0530

    Update split.py

--- plugins/dm/callBack/split.py
+++ plugins/dm/callBack/split.py
@@ -1,45 +1,42 @@
 # fileName : plugins/dm/callBack/split.py
 # copyright ©️ 2021 nabilanavab
+fileName = "plugins/dm/callBack/split.py"
 
-import os, time, shutil
-from pdf import PROCESS
-from logger import logger
-from pyromod import listen
-from plugins.util import *
-from plugins.render import *
-from configs.config import images
-from pyrogram.types import ForceReply
-from PyPDF2 import PdfFileWriter, PdfFileReader
-from plugins.thumbName import thumbName, formatThumb
-from pyrogram import enums, filters, Client as ILovePDF
+import os, time
+from plugins.util    import *
+from plugins.render  import *
+from plugins.work    import work
+from logger          import logger
+from pyromod         import listen
+from configs.config  import images
+from pyrogram.types  import ForceReply
+from plugins.fncta   import thumbName, formatThumb
+from PyPDF2          import PdfFileWriter, PdfFileReader
+from pyrogram        import enums, filters, Client as ILovePDF
 
 split = filters.create(lambda _, __, query: query.data.startswith("split"))
+
 @ILovePDF.on_callback_query(split)
 async def _split(bot, callbackQuery):
     try:
         lang_code = await getLang(callbackQuery.message.chat.id)
-        CHUNK, _ = await translate(text="split", button="document['cancelCB']", lang_code=lang_code)
-        
-        if await header(bot, callbackQuery):
+        if await header(bot, callbackQuery, lang_code=lang_code):
             return
         
-        chat_id = callbackQuery.message.chat.id
-        message_id = callbackQuery.message.id
-        
-        if chat_id in PROCESS:
+        CHUNK, _ = await translate(text="split", button="document['cancelCB']", lang_code=lang_code)
+        cDIR = await work(callbackQuery, "create", False)
+        if not cDIR:
             return await callbackQuery.answer(CHUNK["inWork"])
         await callbackQuery.answer()
         
         data = callbackQuery.data.split("|")[1]
-        process=CHUNK["work"][0] if data == "R" else CHUNK["work"][1]
+        process = CHUNK["work"][0] if data == "R" else CHUNK["work"][1]
         
         if "•" in callbackQuery.message.text:
             known = True; number_of_pages = int(callbackQuery.message.text.split("•")[1])
         else:
             known = False
         
-        # ADD TO PROCESS
-        PROCESS.append(chat_id)
         nabilanavab = True; i = 0
         while(nabilanavab):
             # REQUEST FOR PG NUMBER (MAX. LIMIT 5)
@@ -48,7 +45,7 @@ async def _split(bot, callbackQuery):
                 break
             i += 1
             needPages = await bot.ask(
-                text = CHUNK["pyromodASK_1"], chat_id = chat_id, reply_to_message_id = message_id,
+                text = CHUNK["pyromodASK_1"], chat_id = callbackQuery.message.chat.id, reply_to_message_id = callbackQuery.message.id,
                 filters = filters.text, reply_markup = ForceReply(True, "Eg: 7:86 [start:end] | 7, 8 ,6 [pages]")
             )
             # IF /exit PROCESS CANCEL
@@ -62,9 +59,9 @@ async def _split(bot, callbackQuery):
                 elif len(pageStartAndEnd) == 2:
                     start = pageStartAndEnd[0]; end = pageStartAndEnd[1]
                     try:
-                        start=int(start); end=int(end)
+                        start = int(start); end = int(end)
                     except Exception: pass
-                    if type(start)==int and type(end)==int:
+                    if type(start) == int and type(end) == int:
                         if (1 <= int(pageStartAndEnd[0])):
                             if (int(pageStartAndEnd[0]) < int(pageStartAndEnd[1])):
                                 nabilanavab = False
@@ -83,7 +80,7 @@ async def _split(bot, callbackQuery):
                 if 1 <= len(singlePages) <= 100:
                     for j in singlePages:
                         try:
-                            j=int(j); newList.append(j)
+                            j = int(j); newList.append(j)
                         except Exception: pass
                     if newList != []:
                          nabilanavab = False
@@ -134,35 +131,34 @@ async def _split(bot, callbackQuery):
         
         # nabilanavab == False [No Error]
         if nabilanavab == True:
-            PROCESS.remove(chat_id)
+            return await work(callbackQuery, "delete", False)
         if nabilanavab == False:
-            input_file = f"{message_id}/inPut.pdf"
-            output_file = f"{message_id}/outPut.pdf"
+            input_file = f"{cDIR}/inPut.pdf"
+            output_file = f"{cDIR}/outPut.pdf"
             
-            downloadMessage = await callbackQuery.message.reply(CHUNK["download"], reply_markup=_, quote = True)
-            file_id = callbackQuery.message.reply_to_message.document.file_id
-            fileSize = callbackQuery.message.reply_to_message.document.file_size
+            dlMSG = await callbackQuery.message.reply(CHUNK["download"], reply_markup=_, quote = True)
             downloadLoc = await bot.download_media(
-                message = file_id, file_name = input_file, progress = progress,
-                progress_args = (fileSize, downloadMessage, time.time())
+                message = callbackQuery.message.reply_to_message.document.file_id,
+                file_name = input_file, progress = progress,
+                progress_args = (
+                    callbackQuery.message.reply_to_message.document.file_size, dlMSG, time.time()
+                )
             )
-            if downloadLoc is None:
-                PROCESS.remove(chat_id)
-                return
-            await downloadMessage.edit(CHUNK["completed"], reply_markup=_)
+            if os.path.getsize(downloadLoc) != callbackQuery.message.reply_to_message.document.file_size:    
+                return await work(callbackQuery, "delete", False)
+            
+            await dlMSG.edit(CHUNK["completed"], reply_markup=_)
             if not known:
                 checked, number_of_pages = await checkPdf(input_file, callbackQuery)
                 if not(checked == "pass"):
-                    return await downloadMessage.delete()
+                    return await dlMSG.delete()
             splitInputPdf = PdfFileReader(input_file)
             number_of_pages = int(splitInputPdf.getNumPages())
             
             if not known and data == "R":
                 if not(int(pageStartAndEnd[1]) <= number_of_pages):
                     await callbackQuery.message.reply(CHUNK["error_9"])
-                    PROCESS.remove(chat_id)
-                    shutil.rmtree(f"{message_id}")
-                    return
+                    return await work(callbackQuery, "delete", False)
             
             splitOutput = PdfFileWriter()
             if not known and data == "R":
@@ -185,12 +181,17 @@ async def _split(bot, callbackQuery):
             with open(output_file, "wb") as output_stream:
                 splitOutput.write(output_stream)
             
-            FILE_NAME, FILE_CAPT, THUMBNAIL = await thumbName(callbackQuery.message, callbackQuery.message.reply_to_message.document.file_name)
+            FILE_NAME, FILE_CAPT, THUMBNAIL = await thumbName(
+                callbackQuery.message,
+                callbackQuery.message.reply_to_message.document.file_name
+            )
             if images.PDF_THUMBNAIL != THUMBNAIL:
-                location = await bot.download_media(message = THUMBNAIL, file_name = f"{callbackQuery.message.id}.jpeg")
+                location = await bot.download_media(
+                    message = THUMBNAIL, file_name = f"{cDIR}/thumb.jpeg"
+                )
                 THUMBNAIL = await formatThumb(location)
             
-            await downloadMessage.edit(CHUNK["upload"], reply_markup=_)
+            await dlMSG.edit(CHUNK["upload"], reply_markup = _)
             await callbackQuery.message.reply_chat_action(enums.ChatAction.UPLOAD_DOCUMENT)
             if data.startswith("S"):
                 caption = f"{newList}"
@@ -199,19 +200,12 @@ async def _split(bot, callbackQuery):
             await callbackQuery.message.reply_document(
                 file_name = FILE_NAME, thumb = THUMBNAIL, quote = True, document = output_file,
                 caption = f"{caption}\n\n{FILE_CAPT}", progress = uploadProgress,
-                progress_args = (downloadMessage, time.time())
+                progress_args = (dlMSG, time.time())
             )
-            await downloadMessage.delete()
-            PROCESS.remove(chat_id)
-            try:
-                os.remove(location)
-            except Exception: pass
-            shutil.rmtree(f"{message_id}")
+            await dlMSG.delete()
+            await work(callbackQuery, "delete", False)
     except Exception as e:
-        logger.exception("plugins/dm/callBack/split: %s" %(e), exc_info=True)
-        try:
-            PROCESS.remove(chat_id)
-            shutil.rmtree(f"{message_id}")
-        except Exception: pass
+        logger.exception("🐞 %s: %s" %(fileName, e), exc_info = True)
+        await work(callbackQuery, "delete", False)
 
 # ===================================================================================================================================[NABIL A NAVAB -> TG: nabilanavab]
