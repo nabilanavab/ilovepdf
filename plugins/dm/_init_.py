cb34aa34f231acfeac9cc16e710a8e5d2b78f07d
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-01-16 11:37:19 +0530

    Update _init_.py

--- plugins/dm/_init_.py
+++ plugins/dm/_init_.py
@@ -1,24 +1,26 @@
 # fileName : plugins/dm/_init_.py
 # copyright ©️ 2021 nabilanavab
+fileName = "plugins/dm/_init_.py"
 
-from lang import langList
-from logger import logger
-from plugins.util import *
-from pyrogram import enums
-from configs.log import log
-from pyrogram import filters
-from configs.db import dataBASE
-from plugins.util import getLang
-from lang.__users__ import userLang
-from pyrogram import Client as ILovePDF
-from configs.config import settings, group, dm, images
-from configs.db import BANNED_USR_DB, BANNED_GRP_DB, invite_link
-from pyrogram.types import Message; from pyrogram.errors import UserNotParticipant
+from plugins.util         import *
+from configs.config       import *
+from configs.db           import *
+from configs.log          import log
+from pyrogram             import enums
+from logger               import logger
+from pyrogram.types       import Message
+from plugins.util         import getLang
+from pyrogram             import filters
+from configs.db           import dataBASE
+from lang                 import langList
+from lang.__users__       import userLang
+from pyrogram             import Client as ILovePDF
+from pyrogram.errors      import UserNotParticipant
 
 if dataBASE.MONGODB_URI:
     from database import db
 
-# =======================================================> BANNED USER <========================================================
+# ==============================================================================================> BANNED USER <========================================================
 async def bannedUsers(_, __, message: Message):
     if (message.from_user.id in dm.BANNED_USERS) or (
             (dm.ADMIN_ONLY) and (message.from_user.id not in dm.ADMINS)) or (
@@ -48,31 +50,37 @@ async def bannedUsr(bot, message):
     except Exception as e:
         logger.exception("plugins/_init_: BAN_USER: %s" %(e), exc_info=True)
 
-# ============================================================> BANNED GROUP <==================================================
+# ============================================================> BANNED GROUP <=========================================================================================
 async def bannedGroups(_, __, message: Message):
     if (message.chat.id in group.BANNED_GROUP) or (
             (group.ADMIN_GROUP_ONLY) and (message.chat.id not in group.ADMIN_GROUPS)) or (
                 (dataBASE.MONGODB_URI) and (message.chat.id in BANNED_GRP_DB)):
         return True
     return False
+async def setDb(_, bot, message: Message):
+    if (dataBASE.MONGODB_URI) and (message.chat.id not in GROUPS):
+        await log.newUser(bot, message, False, False)
+        GROUPS.append(message.chat.id)
+    return True
 banned_group=filters.create(bannedGroups)
-@ILovePDF.on_message(filters.group & banned_group & filters.incoming)
+set_db=filters.create(setDb)
+@ILovePDF.on_message(filters.group & set_db & banned_group & filters.incoming)
 async def bannedGrp(bot, message):
     try:
         lang_code = await getLang(message.chat.id)
         await message.reply_chat_action(enums.ChatAction.TYPING)
         if message.chat.id in BANNED_GRP_DB:
-            ban = await db.get_key(id=message.chat.id, key="banned", type="group")
+            ban = await db.get_key(id=message.chat.id, key="banned", typ="group")
             trans_txt, trans_btn = await translate(text="BAN['GroupCantUseDB']", button="BAN['banCB']", lang_code=lang_code)
             toPin = await message.reply_photo(
                 photo = images.BANNED_PIC, reply_markup = trans_btn, quote = True,
-                caption = trans_txt.format(message.chat.title, ban["ban_reason"])
+                caption = trans_txt.format(message.chat.title, ban)
             )
         else:
             trans_txt, trans_btn = await translate(text="BAN['GroupCantUse']", button="BAN['banCB']", lang_code=lang_code)
             toPin = await message.reply_photo(
                 photo = images.BANNED_PIC, reply_markup = trans_btn,
-                caption = GroupCantUse.format(message.chat.title), quote = True
+                caption = trans_txt.format(message.chat.title), quote = True
             )
         try:
             await toPin.pin()
@@ -81,7 +89,7 @@ async def bannedGrp(bot, message):
     except Exception as e:
         logger.exception("plugins/_init_: BANNED_GROUP: %s" %(e), exc_info=True)
 
-# ===================================================>  IF FORCE SUBSCRIPTION  <================================================
+# ====================================>  IF FORCE SUBSCRIPTION  <=====================================================================================================
 async def notSubscribed(_, bot, message: Message):
     if message.text and message.text.startswith("/start"):
         msg = message.text.split(" ")
@@ -104,7 +112,9 @@ async def notSubscribed(_, bot, message: Message):
         return False                             # IF FORCE SUB. TREAT AS SUBSCRIBED
     else:
         try:
-            userStatus = await bot.get_chat_member(str(settings.UPDATE_CHANNEL), message.from_user.id)
+            userStatus = await bot.get_chat_member(
+                str(settings.UPDATE_CHANNEL), message.from_user.id
+            )
             if userStatus.status == 'kicked':    # IF USER BANNED FROM CHANNEL
                 return True
             return False
@@ -118,8 +128,15 @@ async def non_subscriber(bot, message):
     try:
         lang_code = await getLang(message.chat.id)
         await message.reply_chat_action(enums.ChatAction.TYPING)
-        tTXT, tBTN = await translate(text="BAN['Force']", button="BAN['ForceCB']", asString=True, lang_code=lang_code)
-        tBTN = await editDICT(inDir=tBTN, value=invite_link[0])
+        if message.text and message.text.startswith("/start") and "-g" in message.text:
+            msg = message.text.split(" ")[1]
+            code = msg.replace("-l", "-r").split("-r")[0]
+        else:
+            code = ""
+        tTXT, tBTN = await translate(
+            text="BAN['Force']", button="BAN['ForceCB']", asString=True, lang_code=lang_code
+        )
+        tBTN = await editDICT(inDir=tBTN, value=[invite_link[0], code])
         button = await createBUTTON(btn = tBTN, order="11")
         return await message.reply_photo(
             photo = images.WELCOME_PIC, quote = True,
