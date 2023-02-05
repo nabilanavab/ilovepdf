94068781456c04128a8482cb323eac55ac1136e6
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2023-02-05 23:01:26 +0530

    Update __index__.py

--- plugins/dm/callBack/inline_query/__index__.py
+++ plugins/dm/callBack/inline_query/__index__.py
@@ -33,7 +33,7 @@ async def setLang(bot, inline_query):
                 span = ""
                 infos = a.find_all("span")
                 for info in infos:
-                    span += f"{info.text}\n" if info.text != ("•")
+                    span += f"{info.text}\n" if info.text != "•" else ""
                 
                 data[i]["span"] = span
                 data[i]["thumb"] = a.img.get_attribute_list("src")[0]
