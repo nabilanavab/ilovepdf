01b8527f593615cdd4ab94de0e8f61ab46801199
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-02-14 07:08:18 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,13 +1,3 @@
 # Author: @nabilanavab
 
-from .utils   import *
 
-#__all__ = ["work", "render", "fncta", "util"]
-
-
-"""
-  "work",
-  "formatThumb", "thumbName",
-  "createBUTTON", "translate", "getLang", "editDICT",
-  "header", "gSF", "checkPdf", "progress", "uploadProgress", "cbPRO"
-"""
