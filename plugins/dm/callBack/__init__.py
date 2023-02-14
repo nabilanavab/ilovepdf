3ea0c71f31c6194e5a9c6b0e385f6fc2aa301fce
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-02-15 03:27:05 +0530

    Update __init__.py

--- plugins/dm/callBack/__init__.py
+++ plugins/dm/callBack/__init__.py
@@ -4,11 +4,4 @@
 file_name = "plugins/dm/callBack/__init__.py"
 __author_name__ = "Nabil A Navab: @nabilanavab"
 
-
-from time      import time
-from asyncio   import sleep
-
-__all__ = [ "os", "time", "sleep" ]
-
-
 # Author: @nabilanavab
