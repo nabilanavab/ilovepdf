1005e5193e113b766aeaf7257376b8187cd0bdf7
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-02-15 03:52:07 +0530

    Update __init__.py

--- plugins/utils/__init__.py
+++ plugins/utils/__init__.py
@@ -1,4 +1,5 @@
 
 from . import fncta, render, work, util
 
-__all__ = ["work", "render", "fncta", "util"]
+#__all__ = ["work", "render", "fncta", "util"]
+__all__ = [ "createBUTTON", "translate", "getLang" ,"editDICT" ]
