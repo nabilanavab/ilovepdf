ffb628670d02964a3262506cc55941c843d06eab
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-02-15 04:56:29 +0530

    Update settings.py

--- plugins/dm/settings.py
+++ plugins/dm/settings.py
@@ -1,8 +1,11 @@
 # fileName : plugins/dm/settings.py
 # copyright ©️ 2021 nabilanavab
 
+file_name = "plugins/dm/settings.py"
+__author_name__ = "Nabil A Navab: @nabilanavab"
+
 import asyncio
-from plugins.utils               import *
+from plugins.utils          import *
 from logger                 import logger
 from lang.__users__         import userLang
 from configs.db             import dataBASE, myID
@@ -21,7 +24,7 @@ settings = filters.create(lambda _, __, query: query.data.startswith("set"))
 @ILovePDF.on_callback_query(settings)
 async def _settings(bot, callbackQuery):
     try:
-        lang_code = await getLang(callbackQuery.message.chat.id)
+        lang_code = await util.getLang(callbackQuery.message.chat.id)
         data = callbackQuery.data.split("|", 1)[1]
         
         if not(data.startswith("set|lang")) and (
@@ -34,7 +37,7 @@ async def _settings(bot, callbackQuery):
         
         if data == "B":        # Home|B will redirect to settings
             if not dataBASE.MONGODB_URI:
-                defalt, _ = await translate(text="SETTINGS['default'][0]", lang_code=lang_code)
+                defalt, _ = await util.translate(text="SETTINGS['default'][0]", lang_code=lang_code)
                 args = [
                     f"{callbackQuery.from_user.mention}", f"{callbackQuery.from_user.id}",
                     f"@{callbackQuery.from_user.username}" if callbackQuery.from_user.username else "❌",
@@ -44,10 +47,10 @@ async def _settings(bot, callbackQuery):
             else:
                 data = await db.get_user_data(callbackQuery.message.chat.id)
                 if not data:    # if monogo data fetching fails
-                     error, errorCB = await translate(text="SETTINGS['error']", button="SETTINGS['back'][0]", lang_code=lang_code)
+                     error, errorCB = await util.translate(text="SETTINGS['error']", button="SETTINGS['back'][0]", lang_code=lang_code)
                      return await callbackQuery.edit_message_caption(caption=error, reply_markup=errorCB)
                 
-                defalt, _ = await translate(text="SETTINGS['default']", lang_code=lang_code)
+                defalt, _ = await util.translate(text="SETTINGS['default']", lang_code=lang_code)
                 args = [
                     f"{callbackQuery.from_user.mention}", f"{callbackQuery.from_user.id}",
                     f"@{callbackQuery.from_user.username}" if callbackQuery.from_user.username else "❌",
@@ -56,29 +59,29 @@ async def _settings(bot, callbackQuery):
                     f"`{data['fname']}`" if data.get('fname', 0) else defalt[0] ]
                 await callbackQuery.edit_message_media(InputMediaPhoto(data['thumb']) if data.get('thumb', 0) else InputMediaPhoto(images.THUMBNAIL_URL))   #thumb
             
-            tTXT, tBTN = await translate(text="HOME['HomeB']", button="HOME['HomeBCB']", lang_code=lang_code)
+            tTXT, tBTN = await util.translate(text="HOME['HomeB']", button="HOME['HomeBCB']", lang_code=lang_code)
             return await callbackQuery.edit_message_caption(caption=tTXT.format(*args), reply_markup=tBTN)
         
         if data == "B2S":        # from settings part function to settings
-            _, tBTN = await translate(button="HOME['HomeBCB']", lang_code=lang_code)
+            _, tBTN = await util.translate(button="HOME['HomeBCB']", lang_code=lang_code)
             return await callbackQuery.message.edit_reply_markup(tBTN)
         
         if not data.startswith("lang") and not dataBASE.MONGODB_URI:
-            tTXT, tBTN = await translate(text="STATUS_MSG['NO_DB']", lang_code=lang_code)
+            tTXT, tBTN = await util.translate(text="STATUS_MSG['NO_DB']", lang_code=lang_code)
             return await callbackQuery.answer(tTXT)
 
         user_id = callbackQuery.from_user.id; chat_id = callbackQuery.message.chat.id; chat_type = callbackQuery.message.chat.type
         
         if data.startswith("lang"):    # getLang
             if not set.MULTI_LANG_SUP:
-                cant, _ = await translate(text="SETTINGS['cant']", lang_code=lang_code)
+                cant, _ = await util.translate(text="SETTINGS['cant']", lang_code=lang_code)
                 return await callbackQuery.answer(cant)
             if data == "lang":
-                change, _ = await translate(text="SETTINGS['chgLang']", lang_code=lang_code)
+                change, _ = await util.translate(text="SETTINGS['chgLang']", lang_code=lang_code)
                 _lang = { langList[lang][1]:f"set|lang|{lang}" for lang in langList if lang != lang_code }
-                change.update(_lang); back, _ = await translate(text="SETTINGS['back'][1]", lang_code=lang_code)
-                change.update(back); change = await createBUTTON(btn=change, order=int(f"1{((len(change)-2)//3)*'3'}{(len(change)-2)%3}1"))
-                tTXT, _ = await translate(text="SETTINGS['lang']", lang_code=lang_code); await callbackQuery.answer(tTXT)
+                change.update(_lang); back, _ = await util.translate(text="SETTINGS['back'][1]", lang_code=lang_code)
+                change.update(back); change = await util.createBUTTON(btn=change, order=int(f"1{((len(change)-2)//3)*'3'}{(len(change)-2)%3}1"))
+                tTXT, _ = await util.translate(text="SETTINGS['lang']", lang_code=lang_code); await callbackQuery.answer(tTXT)
                 return await callbackQuery.message.edit_reply_markup(change)
             else:
                 lang = data.split("|", 1)[1]; userLang[chat_id] = lang
@@ -91,19 +94,19 @@ async def _settings(bot, callbackQuery):
                         await db.dlt_key(id=callbackQuery.message.chat.id, key="lang")
                     elif chat_type != ChatType.PRIVATE and lang != set.DEFAULT_LANG:
                         await db.dlt_key(id=callbackQuery.message.chat.id, key="lang", typ="group")
-            _, __ = await translate(text="SETTINGS['feedback']", button="SETTINGS['feedbtn']", lang_code=lang)
+            _, __ = await util.translate(text="SETTINGS['feedback']", button="SETTINGS['feedbtn']", lang_code=lang)
             await callbackQuery.message.reply_text(text=_.format(await disLang(lang)), reply_markup=__)
         
         elif data.startswith("thumb"):
             if data == "thumb":
                 if user_id in CUSTOM_THUMBNAIL_U:
-                    _, tBTN = await translate(button="SETTINGS['thumb'][1]", order=121, lang_code=lang_code)
+                    _, tBTN = await util.translate(button="SETTINGS['thumb'][1]", order=121, lang_code=lang_code)
                     return await callbackQuery.message.edit_reply_markup(tBTN)
                 else:
-                    _, tBTN = await translate(button="SETTINGS['thumb'][0]", order=111, lang_code=lang_code)
+                    _, tBTN = await util.translate(button="SETTINGS['thumb'][0]", order=111, lang_code=lang_code)
                     return await callbackQuery.message.edit_reply_markup(tBTN)
             elif data == "thumb+":    # add thumb
-                tTXT, tBTN = await translate(text="SETTINGS['ask']", button="SETTINGS['wait']", order=111, lang_code=lang_code)
+                tTXT, tBTN = await util.translate(text="SETTINGS['ask']", button="SETTINGS['wait']", order=111, lang_code=lang_code)
                 await callbackQuery.edit_message_caption(caption=tTXT[0], reply_markup=tBTN)
                 await asyncio.sleep(1)
                 await callbackQuery.edit_message_caption(caption=tTXT[1], reply_markup=tBTN)
@@ -120,15 +123,15 @@ async def _settings(bot, callbackQuery):
             current = DATA.get(int(user_id), 0)
             if data == "api":        # DATA saved order ["api", "fname", "caption]
                 if current and current[0]:
-                    _, tBTN = await translate(button="SETTINGS['api'][1]", order=121, lang_code=lang_code)
+                    _, tBTN = await util.translate(button="SETTINGS['api'][1]", order=121, lang_code=lang_code)
                     return await callbackQuery.message.edit_reply_markup(tBTN)
                 else:
-                    _, tBTN = await translate(button="SETTINGS['api'][0]", order=111, lang_code=lang_code)
+                    _, tBTN = await util.translate(button="SETTINGS['api'][0]", order=111, lang_code=lang_code)
                     return await callbackQuery.message.edit_reply_markup(tBTN)
             elif data == "api+":
-                tTXT, tBTN = await translate(text="SETTINGS['ask']", button="SETTINGS['waitApi']", order=111, lang_code=lang_code)
+                tTXT, tBTN = await util.translate(text="SETTINGS['ask']", button="SETTINGS['waitApi']", order=111, lang_code=lang_code)
                 await callbackQuery.edit_message_caption(caption=tTXT[0], reply_markup=tBTN)
-                _, __ = await translate(text="SETTINGS['askApi']", lang_code=lang_code)
+                _, __ = await util.translate(text="SETTINGS['askApi']", lang_code=lang_code)
                 await asyncio.sleep(1)
                 await callbackQuery.edit_message_caption(caption=tTXT[1]+_, reply_markup=tBTN)
                 getName = await bot.listen(user_id)
@@ -145,18 +148,18 @@ async def _settings(bot, callbackQuery):
         
         elif data.startswith("fname"):
             if set.DEFAULT_NAME:
-                cant, _ = await translate(text="SETTINGS['cant']", lang_code=lang_code)
+                cant, _ = await util.translate(text="SETTINGS['cant']", lang_code=lang_code)
                 return await callbackQuery.answer(cant)
             current = DATA.get(int(user_id), 0)
             if data == "fname":        # DATA saved order ["api", "fname", "caption]
                 if current and current[1]:
-                    _, tBTN = await translate(button="SETTINGS['fname'][1]", order=121, lang_code=lang_code)
+                    _, tBTN = await util.translate(button="SETTINGS['fname'][1]", order=121, lang_code=lang_code)
                     return await callbackQuery.message.edit_reply_markup(tBTN)
                 else:
-                    _, tBTN = await translate(button="SETTINGS['fname'][0]", order=111, lang_code=lang_code)
+                    _, tBTN = await util.translate(button="SETTINGS['fname'][0]", order=111, lang_code=lang_code)
                     return await callbackQuery.message.edit_reply_markup(tBTN)
             elif data == "fname+":
-                tTXT, tBTN = await translate(text="SETTINGS['ask']", button="SETTINGS['wait']", order=111, lang_code=lang_code)
+                tTXT, tBTN = await util.translate(text="SETTINGS['ask']", button="SETTINGS['wait']", order=111, lang_code=lang_code)
                 await callbackQuery.edit_message_caption(caption=tTXT[0], reply_markup=tBTN)
                 await asyncio.sleep(1)
                 await callbackQuery.edit_message_caption(caption=tTXT[1], reply_markup=tBTN)
@@ -174,18 +177,18 @@ async def _settings(bot, callbackQuery):
         
         elif data.startswith("capt"):
             if set.DEFAULT_CAPT:
-                cant, _ = await translate(text="SETTINGS['cant']", lang_code=lang_code)
+                cant, _ = await util.translate(text="SETTINGS['cant']", lang_code=lang_code)
                 return await callbackQuery.answer(cant)
             current = DATA.get(int(user_id), 0)
             if data == "capt":        # DATA saved order ["api", "fname", "caption]
                 if current and current[2]:
-                    _, tBTN = await translate(button="SETTINGS['capt'][1]", order=121, lang_code=lang_code)
+                    _, tBTN = await util.translate(button="SETTINGS['capt'][1]", order=121, lang_code=lang_code)
                     return await callbackQuery.message.edit_reply_markup(tBTN)
                 else:
-                    _, tBTN = await translate(button="SETTINGS['capt'][0]", order=111, lang_code=lang_code)
+                    _, tBTN = await util.translate(button="SETTINGS['capt'][0]", order=111, lang_code=lang_code)
                     return await callbackQuery.message.edit_reply_markup(tBTN)
             elif data == "capt+":
-                tTXT, tBTN = await translate(text="SETTINGS['ask']", button="SETTINGS['wait']", order=111, lang_code=lang_code)
+                tTXT, tBTN = await util.translate(text="SETTINGS['ask']", button="SETTINGS['wait']", order=111, lang_code=lang_code)
                 await callbackQuery.edit_message_caption(caption=tTXT[0], reply_markup=tBTN)
                 await asyncio.sleep(1)
                 await callbackQuery.edit_message_caption(caption=tTXT[1], reply_markup=tBTN)
@@ -202,17 +205,17 @@ async def _settings(bot, callbackQuery):
                 await db.dlt_key(callbackQuery.message.chat.id, "capt")
         
         if not data.endswith("+"):    # for successful cbMGS, add wait for use & cb expires
-            result, _ = await translate(text="SETTINGS['result'][1]", lang_code=lang_code)
+            result, _ = await util.translate(text="SETTINGS['result'][1]", lang_code=lang_code)
             await callbackQuery.answer(result)
         if callbackQuery.message.chat.type == ChatType.PRIVATE:
             await callbackQuery.edit_message_media(InputMediaPhoto(images.WELCOME_PIC))
-            tTXT, tBTN = await translate(
+            tTXT, tBTN = await util.translate(
                 text = "HOME['HomeA']", lang_code=lang if data.startswith("lang") else lang_code,
                 button = "HOME['HomeACB']" if callbackQuery.message.chat.id not in dm.ADMINS else "HOME['HomeAdminCB']",
                 order = 2121 if callbackQuery.message.chat.id not in dm.ADMINS else 21221
             )
         else:
-            tTXT, tBTN = await translate(
+            tTXT, tBTN = await util.translate(
                 text="HomeG['HomeA']", button="HomeG['HomeACB']",
                 lang_code=lang if data.startswith("lang") else lang_code
             )
@@ -222,6 +225,4 @@ async def _settings(bot, callbackQuery):
         )
     
     except Exception as e:
-        logger.debug(f"plugins/dm/callBack/settings : {e}", exc_info=True)
-
-# ===================================================================================================================================[NABIL A NAVAB -> TG: nabilanavab]
+        logger.exception("🐞 %s: %s" %(file_name, e), exc_info = True)
