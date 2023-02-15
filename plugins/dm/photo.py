aaf032589ab62a3c526107594f78aa53b2449e5f
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-02-15 05:58:42 +0530

    Update photo.py

--- plugins/dm/photo.py
+++ plugins/dm/photo.py
@@ -1,18 +1,23 @@
 # fileName : plugins/dm/photo.py
 # copyright ©️ 2021 nabilanavab
 
+file_name = "plugins/dm/photo.py"
+__author_name__ = "Nabil A Navab: @nabilanavab"
+
+# LOGGING INFO: DEBUG
+from logger           import logger
+
 HD = {}
 
 import os
-from pdf import PDF
-from PIL import Image
-from logger import logger
-from .generate import _GEN
-from configs.config import settings
-from pyrogram import Client as ILovePDF, filters, enums
-from plugins.utils import *
+from plugins.utils     import *
+from pdf               import PDF
+from .generate         import _GEN
+from PIL               import Image
+from configs.config    import settings
+from pyrogram          import Client as ILovePDF, filters, enums
 
-# ===============================| REPLY TO /HD |======================================================================================================================
+# =========================================| REPLY TO /HD |=======================================
 @ILovePDF.on_message((filters.private | filters.group) & filters.command("hd") & filters.incoming)
 async def _hd(bot, message):
     try:
@@ -21,15 +26,17 @@ async def _hd(bot, message):
         if message.chat.id in HD:
             return await message.delete()
         await message.reply_chat_action(enums.ChatAction.TYPING)
-        lang_code = await getLang(message.chat.id)
-        tTXT, tBTN = await translate(text="document['setHdImg']", button="document['setDefault']", lang_code=lang_code)
-        imageReply = await message.reply_text(text=tTXT, reply_markup=tBTN, quote=True)
+        lang_code = await util.getLang(message.chat.id)
+        tTXT, tBTN = await util.translate(
+            text = "document['setHdImg']", button = "document['setDefault']", lang_code = lang_code
+        )
+        imageReply = await message.reply_text(text = tTXT, reply_markup = tBTN, quote = True)
         HD[message.chat.id] = [imageReply.id]
         return await message.delete()
     except Exception as e:
-        logger.debug("plugins/dm/photo: %s" %(e), exc_info=True)
+        logger.exception("1️⃣: 🐞 %s: %s" %(file_name, e), exc_info = True)
 
-# =======================================================================================================| REPLY TO PHOTOS |===========================================
+# ============================| REPLY TO PHOTOS |======================
 @ILovePDF.on_message(filters.photo & filters.private & filters.incoming)
 async def images(bot, message):
     try:
@@ -43,11 +50,16 @@ async def images(bot, message):
                 return
             HD[message.chat.id].append(message.photo.file_id)
             generateCB = "document['generate']" if settings.DEFAULT_NAME else  "document['generateRN']"
-            tTXT, tBTN = await translate(text="document['imageAdded']", button=generateCB, lang_code=lang_code)
-            return await message.reply_text(tTXT.format(len(HD[message.chat.id])-1, message.chat.id)+" [HD] 🔰", reply_markup=tBTN, quote=True)
+            tTXT, tBTN = await util.translate(
+                text = "document['imageAdded']", button = generateCB, lang_code = lang_code
+            )
+            return await message.reply_text(
+                tTXT.format(len(HD[message.chat.id])-1, message.chat.id)+" [HD] 🔰",
+                reply_markup = tBTN, quote = True
+            )
         
-        tTXT, tBTN = await translate(text="PROGRESS['dlImage']", lang_code=lang_code)
-        imageReply = await message.reply_text(tTXT, quote=True)
+        tTXT, tBTN = await util.translate(text = "PROGRESS['dlImage']", lang_code = lang_code)
+        imageReply = await message.reply_text(tTXT, quote = True)
         if not isinstance(PDF.get(message.chat.id), list):
             PDF[message.chat.id] = []
         await message.download(f"work/{message.chat.id}/{message.chat.id}.jpg")
@@ -56,11 +68,13 @@ async def images(bot, message):
         ).convert("RGB")
         PDF[message.chat.id].append(img)
         generateCB = "document['generate']" if settings.DEFAULT_NAME else  "document['generateRN']"
-        tTXT, tBTN = await translate(text="document['imageAdded']", button=generateCB, lang_code=lang_code)
+        tTXT, tBTN = await util.translate(
+            text = "document['imageAdded']", button = generateCB, lang_code = lang_code
+        )
         await imageReply.edit(
             tTXT.format(len(PDF[message.chat.id]), message.chat.id), reply_markup = tBTN
         )
     except Exception as e:
-        logger.exception("plugins/dm/photo: %s" %(e), exc_info=True)
+        logger.exception("2️⃣: 🐞 %s: %s" %(file_name, e), exc_info = True)
 
-# ===================================================================================================================================[NABIL A NAVAB -> TG: nabilanavab]
+ # Author: @nabilanavab
