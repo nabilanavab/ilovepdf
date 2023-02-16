b34b27036c67cbd90d63fcdc89d4840119f4bae1
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-02-16 23:08:47 +0530

    Update encryptPDF.py

--- plugins/dm/callBack/file_process/encryptPDF.py
+++ plugins/dm/callBack/file_process/encryptPDF.py
@@ -11,25 +11,22 @@ import fitz
 
 async def encryptPDF(input_file: str, password: str, cDIR: str) -> Tuple[ bool, str ]:
     try:
-        try:
-            output_path = f"{cDIR}/outPut.pdf"
-            with fitz.open(input_file) as iNPUT:
-                number_of_pages = iNPUT.page_count
-                iNPUT.save(
-                    output_path,
-                    encryption = fitz.PDF_ENCRYPT_AES_256, # strongest algorithm
-                    owner_pw = "nabil",
-                    user_pw = f"{password.text}",
-                    permissions = int(
-                        fitz.PDF_PERM_ACCESSIBILITY |
-                        fitz.PDF_PERM_PRINT |
-                        fitz.PDF_PERM_COPY |
-                        fitz.PDF_PERM_ANNOTATE
-                    )
+        output_path = f"{cDIR}/outPut.pdf"
+        with fitz.open(input_file) as iNPUT:
+            number_of_pages = iNPUT.page_count
+            iNPUT.save(
+                output_path,
+                encryption = fitz.PDF_ENCRYPT_AES_256, # strongest algorithm
+                owner_pw = "nabil",
+                user_pw = f"{password.text}",
+                permissions = int(
+                    fitz.PDF_PERM_ACCESSIBILITY |
+                    fitz.PDF_PERM_PRINT |
+                    fitz.PDF_PERM_COPY |
+                    fitz.PDF_PERM_ANNOTATE
                 )
-            return True, output_path
-        except Exception as Error:
-            return False, Error
+            )
+        return True, output_path
         
     except Exception as Error:
         logger.exception("🐞 %s: %s" %(file_name, Error), exc_info = True)
