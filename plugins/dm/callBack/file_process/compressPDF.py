acaf7a0d1af23e965e5cc978eb5da1da534069f2
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-02-16 22:48:59 +0530

    Create compressPDF.py

---
+++ plugins/dm/callBack/file_process/compressPDF.py
@@ -0,0 +1,42 @@
+# fileName : plugins/dm/callBack/file_process/drawPDF.py
+# copyright ©️ 2021 nabilanavab
+
+file_name = "plugins/dm/callBack/file_process/drawPDF.py"
+__author_name__ = "Nabil A Navab: @nabilanavab"
+
+# LOGGING INFO: DEBUG
+from logger import logger
+
+import fitz
+from utils                       import *
+from PDFNetPython3.PDFNetPython  import PDFDoc, Optimizer, SDFDoc, PDFNet
+
+async def darwPDF(input_file: str, cDIR: str) -> Tuple[ bool, str ]:
+    try:
+        output_path = f"{cDIR}/outPut.pdf"
+        # Initialize the library
+        PDFNet.Initialize()
+        doc = PDFDoc(input_file)
+        # Optimize PDF with the default settings
+        doc.InitSecurityHandler()
+        # Reduce PDF size by removing redundant information and
+        # compressing data streams
+        Optimizer.Optimize(doc)
+        doc.Save(output_path, SDFDoc.e_linearized)
+        doc.Close()
+        
+        # FILE SIZE COMPARISON (RATIO)
+        initialSize = os.path.getsize(input_file)
+        compressedSize = os.path.getsize(output_path)
+        ratio = (1 - (compressedSize / initialSize)) * 100
+        
+        if (initialSize - compressedSize) > 1000000 or ratio >= 5:
+            return await render.gSF(initialSize), await render.gSF(compressedSize), ratio
+        else:
+            return True, output_path
+    
+    except Exception as Error:
+        logger.exception("🐞 %s: %s" %(file_name, Error), exc_info = True)
+        return False, Error
+
+# Author: @nabilanavab
