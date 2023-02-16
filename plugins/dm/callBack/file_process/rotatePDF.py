50c7505c4c098028bfee0c66b72c1cc468eabfee
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-02-16 23:20:47 +0530

    Create rotatePDF.py

---
+++ plugins/dm/callBack/file_process/rotatePDF.py
@@ -0,0 +1,29 @@
+# fileName : plugins/dm/callBack/file_process/encryptPDF.py
+# copyright ©️ 2021 nabilanavab
+
+file_name = "plugins/dm/callBack/file_process/encryptPDF.py"
+__author_name__ = "Nabil A Navab: @nabilanavab"
+
+# LOGGING INFO: DEBUG
+from logger import logger
+
+import fitz
+
+async def encryptPDF(input_file: str, angle: str, cDIR: str) -> Tuple[ bool, str ]:
+    try:
+        output_path = f"{cDIR}/outPut.pdf"
+        with fitz.open(input_file) as iNPUT:
+            if angle == "rot90":
+                for page in doc: page.set_rotation(90)
+            elif angle == "rot180":
+                for page in doc: page.set_rotation(180)
+            elif angle == "rot270":
+                for page in doc: page.set_rotation(-90)
+            doc.save(output_path)
+        return True, output_path
+        
+    except Exception as Error:
+        logger.exception("🐞 %s: %s" %(file_name, Error), exc_info = True)
+        return False, Error
+
+# Author: @nabilanavab
