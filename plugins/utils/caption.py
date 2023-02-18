1431d1461488fa8937b5758fc2f9a8f5d61f7c31
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-02-18 07:15:49 +0530

    Update caption.py

--- plugins/utils/caption.py
+++ plugins/utils/caption.py
@@ -8,8 +8,9 @@ __author_name__ = "Nabil A Navab: @nabilanavab"
 from logger           import logger
 
 from plugins.util     import util
+from configs.config   import settings
 
-async def caption(data: str, lang_code :str = ) -> str:
+async def caption(data: str, lang_code :str = settings.DEFAULT_LANG) -> str:
     try:
         if data == "encrypt":
             caption = "common['encrypt_caption']"
