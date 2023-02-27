c6e3c94355d24873be73ea3c38f4c440a0fbc66b
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-02-27 06:31:13 +0530

    Update __init__.py

--- plugins/dm/callBack/file_process/__init__.py
+++ plugins/dm/callBack/file_process/__init__.py
@@ -1,17 +1,17 @@
-from .   import *
+# fileName : plugins/dm/callBack/__init__.py
+# copyright ©️ 2021 nabilanavab
+
+file_name = "plugins/dm/callBack/__init__.py"
+__author_name__ = "Nabil A Navab: @nabilanavab"
+
+from . import *
 
 __all__ = [
-    "encryptPDF",
-    "decryptPDF",
-    "blackAndWhitePdf",
-    "combinePages",
-    "compressPDF",
-    "decryptPDF",
-    "drawPDF",
-    "formatPDF",
-    "ocrPDF",
-    "rotatePDF",
-    "saturatePDF",
-    "textPDF",
-    "zoomPDF"
+    "textPDF", "zoomPDF",
+    "encryptPDF", "decryptPDF",
+    "blackAndWhitePdf", "combinePages",
+    "compressPDF", "decryptPDF", "drawPDF",
+    "formatPDF", "ocrPDF", "rotatePDF", "saturatePDF",
 ]
+
+# Author: @nabilanavab
