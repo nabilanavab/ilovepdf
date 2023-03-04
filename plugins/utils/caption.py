34f009e87736422fe6997bcca8c248abf6868703
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-05 02:16:33 +0530

    Update caption.py

--- plugins/utils/caption.py
+++ plugins/utils/caption.py
@@ -11,7 +11,7 @@ from plugins.utils    import util
 from configs.config   import settings
 
 async def caption(
-    data: str, lang_code :str = settings.DEFAULT_LANG, args
+    data: str, lang_code :str = settings.DEFAULT_LANG, args = None
 ) -> str:
     try:
         """ return caption deepending upon the work """
