946134aadf0158f77ec83dd21d34e05211b1dca9
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-04 15:58:22 +0530

    Update rotatePDF.py

--- plugins/dm/callBack/file_process/rotatePDF.py
+++ plugins/dm/callBack/file_process/rotatePDF.py
@@ -1,7 +1,7 @@
-# fileName : plugins/dm/callBack/file_process/encryptPDF.py
+# fileName : plugins/dm/callBack/file_process/rotatePDF.py
 # copyright ©️ 2021 nabilanavab
 
-file_name = "plugins/dm/callBack/file_process/encryptPDF.py"
+file_name = "plugins/dm/callBack/file_process/rotatePDF.py"
 __author_name__ = "Nabil A Navab: @nabilanavab"
 
 # LOGGING INFO: DEBUG
@@ -9,7 +9,19 @@ from logger import logger
 
 import fitz
 
-async def encryptPDF(input_file: str, angle: str, cDIR: str) -> ( bool, str ):
+async def rotatePDF(input_file: str, angle: str, cDIR: str) -> ( bool, str ):
+    """
+    If a PDF file is rotated incorrectly, rotating it can help to correct the orientation of the pages so that they are displayed properly.
+    Rotating a PDF file can also allow you to change the viewing angle of the document, which can be helpful when examining images or graphics.
+    
+    parameter:
+        input_file : Here is the path of the file that the user entered
+        cDIR       : This is the location of the directory that belongs to the specific user.
+    
+    return:
+        bool        : Return True when the request is successful
+        output_path : This is the path where the output file can be found.
+    """
     try:
         output_path = f"{cDIR}/outPut.pdf"
         with fitz.open(input_file) as iNPUT:
