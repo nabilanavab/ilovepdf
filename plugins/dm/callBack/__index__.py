e28c5560396d1689975877437e116b353bee37b5
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-05 10:35:01 +0530

    Update __index__.py

--- plugins/dm/callBack/__index__.py
+++ plugins/dm/callBack/__index__.py
@@ -70,7 +70,7 @@ async def __index__(bot, callbackQuery):
             if not notExit:
                 await work.work(callbackQuery, "delete", False)
                 return await newName.reply(CHUNK["exit"], quote = True)
-        elif data in ["Images", "document"]:
+        elif data.startswith("#p2img"):
             notExit, imageList = await renamePDF.askName(
                 bot, callbackQuery, question = CHUNK["askImage"],
                 limit = callbackQuery.message.text.split("•")[1] if "•" in callbackQuery.message.text else 1000
