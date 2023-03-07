90f2c9b9d04d85ac2c0727b3a3683a643bbb2e48
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-07 11:29:55 +0530

    Update __index__.py

--- plugins/dm/callBack/__index__.py
+++ plugins/dm/callBack/__index__.py
@@ -71,7 +71,7 @@ async def __index__(bot, callbackQuery):
                 await work.work(callbackQuery, "delete", False)
                 return await newName.reply(CHUNK["exit"], quote = True)
         # ends with a means all pages.. so no questions
-        elif data.startswith("p2img") and not data.endswith("A"):
+        elif (data.startswith("p2img") or data.startswith("split")) and not data.endswith("A"):
             notExit, imageList = await pdfToImages.askimageList(
                 bot, callbackQuery, question = CHUNK["askImage"],
                 limit = int(callbackQuery.message.text.split("•")[1]) if "•" in callbackQuery.message.text else 1000
