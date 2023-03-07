977ba853e1c5f8ca69d1f6cfd4e60131299da684
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-07 18:27:40 +0530

    Update mergePDF.py

--- plugins/dm/callBack/file_process/mergePDF.py
+++ plugins/dm/callBack/file_process/mergePDF.py
@@ -27,7 +27,7 @@ async def askPDF(bot, callbackQuery, question: str, size: str) -> ( bool, list )
             input_file = await bot.ask(
                 chat_id = callbackQuery.from_user.id,
                 reply_to_message_id = callbackQuery.message.id,
-                text = question.format(len(mergeId + 1)), filters = None,
+                text = question.format(len(mergeId)+1), filters = None,
             )
             if input_file.text == "/exit":
                 return false, input_file
