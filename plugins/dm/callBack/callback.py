f3f46d738b8a67625f6c3c2b6df756112da22f6f
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-10 03:38:11 +0530

    Update callback.py

--- plugins/dm/callBack/callback.py
+++ plugins/dm/callBack/callback.py
@@ -124,6 +124,31 @@ async def _pdf(bot, callbackQuery):
     except Exception as e:
         logger.exception("🐞 %s: %s" %(file_name, e), exc_info = True)
 
+
+aio = filters.create(lambda _, __, query: query.data.startswith("aio"))
+@ILovePDF.on_callback_query(aio)
+async def _aio(bot, callbackQuery):
+    try:
+        lang_code = await util.getLang(callbackQuery.message.chat.id)
+        if await render.header(bot, callbackQuery, lang_code=lang_code):
+            return
+        
+        await callbackQuery.answer()
+        data = data.split("|", 1)[1]
+        
+        if data == "aio":
+            tTXT, tBTN = await util.translate(text="AIO['aio']", button = "AIO['aio_button']", order = 22222221, lang_code = lang_code)
+            return await callbackQuery.message.edit(
+                text=tTXT.format(callbackQuery.message.reply_to_message.document.file_name, 
+                                 await render.gSF(callbackQuery.message.reply_to_message.document.file_size)),
+                reply_markup = tBTN
+            )
+        
+    except Exception as Error:
+        logger.exception("🐞 %s: %s" %(file_name, Error), exc_info = True)
+
+
+
 common = filters.create(lambda _, __, query: query.data.startswith("-|"))
 
 @ILovePDF.on_callback_query(common)
