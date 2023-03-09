b6b3fbf2e3177b8a2b6d0d07c9c14b803daaa8e3
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-09 05:39:51 +0530

    Update __index__.py

--- plugins/dm/callBack/__index__.py
+++ plugins/dm/callBack/__index__.py
@@ -148,7 +148,7 @@ async def __index__(bot, callbackQuery):
             isSuccess, output_file = await twoPagesToOne.twoPagesToOne(cDIR = cDIR, input_file = input_file)
         
         elif data == "3-format":
-            isSuccess, output_file = await formatPDF.formatPDF(cDIR = cDIR, input_file = input_file)
+            isSuccess, output_file = await threePagesToOne.threePagesToOne(cDIR = cDIR, input_file = input_file)
         
         elif data == "4-format":
             isSuccess, output_file = await combinePages.combinePages(cDIR = cDIR, input_file = input_file)
