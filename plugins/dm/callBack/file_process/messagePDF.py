a0dbd66be58bf50d38344e2ef7451c3138ecfd1f
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-09 16:30:12 +0530

    Update messagePDF.py

--- plugins/dm/callBack/file_process/messagePDF.py
+++ plugins/dm/callBack/file_process/messagePDF.py
@@ -7,7 +7,7 @@ __author_name__ = "Nabil A Navab: @nabilanavab"
 # LOGGING INFO: DEBUG
 from logger import logger
 
-import fitz
+import fitz, asyncio
 from plugins.utils         import *
 from pyrogram.errors       import FloodWait
 
