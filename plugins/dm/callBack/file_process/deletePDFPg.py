8a4e3b4bc8aba1674b90076d633228e3685fce7c
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-09 07:15:17 +0530

    Create deletePDFPg.py

---
+++ plugins/dm/callBack/file_process/deletePDFPg.py
@@ -0,0 +1,38 @@
+# fileName : plugins/dm/callBack/file_process/deletePDFPg.py
+# copyright ©️ 2021 nabilanavab
+
+file_name = "plugins/dm/callBack/file_process/deletePDFPg.py"
+__author_name__ = "Nabil A Navab: @nabilanavab"
+
+# LOGGING INFO: DEBUG
+from logger import logger
+
+from fitz
+
+async def deletePDFPg(input_file: str, cDIR: str, imageList: list) -> ( bool, str):
+    """
+     Delete specified pages from a PDF file and save the modified PDF to a new file.
+    
+    parameter:
+        input_file    : Here is the path of the file that the user entered
+        cDIR          : This is the location of the directory that belongs to the specific user.
+        imageList     : List of page numbers that the user want to delete
+        
+    return:
+        bool        : Return True when the request is successful
+        output_path : This is the path where the output file can be found.
+    """
+    try:
+        output_path = f"{cDIR}/outPut.pdf"
+        
+        with fitz.open(input_file) as iNPUT:
+            del iNPUT[imageList]
+            iNPUT.sasve(output_path)
+        
+        return True, output_path
+    
+    except Exception as Error:
+        logger.exception("🐞 %s: %s" %(file_name, Error), exc_info = True)
+        return False, Error
+
+# Author: @nabilanavab
