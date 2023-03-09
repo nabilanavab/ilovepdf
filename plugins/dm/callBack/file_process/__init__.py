f9642c09c9a2d77f0e77114a2acbff2ad79801d6
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-09 05:40:09 +0530

    Update __init__.py

--- plugins/dm/callBack/file_process/__init__.py
+++ plugins/dm/callBack/file_process/__init__.py
@@ -14,7 +14,7 @@ __all__ = [
     "compressPDF", "decryptPDF", "drawPDF",
     "formatPDF", "ocrPDF", "rotatePDF", "saturatePDF",
     "renamePDF", "pdfToImages", "splitPDF", "mergePDF", "watermarkPDF",
-    "stampPDF", "twoPagesToOne"
+    "stampPDF", "twoPagesToOne", "threePagesToOne"
 ]
 
 # Author: @nabilanavab
