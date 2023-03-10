1204e58736682a9d5e03f3a4cf8ae3f59dbe62d2
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-10 10:20:28 +0530

    Update callback.py

--- plugins/dm/callBack/callback.py
+++ plugins/dm/callBack/callback.py
@@ -166,7 +166,8 @@ async def _aio(bot, callbackQuery):
         
         data = data.split("|", 1)[1]
         buttons = callbackQuery.message.reply_markup.inline_keyboard
-        callback = [element.callback_data for button in buttons for index, element in enumerate(button, start=1) if index/2==0 ]
+        #callback = [element.callback_data for button in buttons for element in button ]
+        callback = [element.callback_data for index, button in enumerate(buttons, start=1) if index/2 == 0 for element in button ]
         logger.debug(callback)
         
     except Exception as Error:
