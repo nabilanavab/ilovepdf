3c74ff5285bc85beb5a331885398ed059f45d6a9
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-11 04:09:40 +0530

    Update callback.py

--- plugins/dm/callBack/callback.py
+++ plugins/dm/callBack/callback.py
@@ -177,7 +177,7 @@ async def _aio(bot, callbackQuery):
         buttons = callbackQuery.message.reply_markup.inline_keyboard
         callback = [element.callback_data for button in buttons for index, element in enumerate(button, start=1) if index % 2 == 0]
         logger.debug(callback)
-        all_data = [ False if element.endswith("F") else True for element in callback ]
+        all_data = [ False if element.endswith('F', '{F}') else True for element in callback ]
         dataARRANGEMENT = { "met" : 0, "enc" : 1, "for" : 2, "com" : 3, "wat" : 4, "rnm" : 5 }
         
         logger.debug(all_data)
