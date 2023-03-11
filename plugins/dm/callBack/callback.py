108c737a27bfcd54071f0c60b6cc7e1c659ae868
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-11 13:46:08 +0530

    Update callback.py

--- plugins/dm/callBack/callback.py
+++ plugins/dm/callBack/callback.py
@@ -185,9 +185,7 @@ async def _aio(bot, callbackQuery):
                 reply_markup = InlineKeyboardMarkup(aio_list_btn)
             )
         
-        message_data = callbackQuery.message
-        logger.debug(message_data)
-        message_data = callbackQuery.message.split(tuple("`","`"))
+        message_data = callbackQuery.message.text.split(tuple("`","`"))
         logger.debug(message_data)
         
         # data1 = "meta/enc/form/comp/water/n.." , data2 = "{}/True/False"
