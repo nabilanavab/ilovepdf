1cfaf1839855f7f8a413d838b419a13954308570
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-11 18:19:54 +0530

    Create beta.py

---
+++ configs/beta.py
@@ -0,0 +1,15 @@
+# fileName : plugins/dm/document.py
+# copyright ©️ 2021 nabilanavab
+
+file_name = "plugins/dm/document.py"
+__author_name__ = "Nabil A Navab: @nabilanavab"
+
+"""
+    A list of user IDs can be used to identify a group of beta users,
+    who are typically a subset of users selected to test new features
+    or products before they are released to the general public.
+"""
+
+BETA = [ 531733867 ,]
+
+# Author: @nabilanavab
