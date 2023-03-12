92c35c8c23d3ad2a755129c805128c9738d1dcb9
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-12 09:34:15 +0530

    Update callback.py

--- plugins/dm/callBack/callback.py
+++ plugins/dm/callBack/callback.py
@@ -152,14 +152,12 @@ async def _aio(bot, callbackQuery):
             if data == "aioInput|enc":
                 #tBTN = await util.createBUTTON(btn=tTXT['waitPASS'])
                 #await callbackQuery.message.edit_reply_markup(tBTN)
-                (qn, input_str) = await bot.ask(
+                input_str = await bot.ask(
                     text = tTXT['waitPASS'], chat_id = callbackQuery.from_user.id,
                     reply_to_message_id = callbackQuery.message.id, reply_markup = ForceReply(True, tTXT['waitPASS'])
                 )
-                while not input_str.text:
-                    await input_str.delete()
-                    input_str = await bot.listen(chat_id = callbackQuery.from_user.id)
-                await qn.delete()
+                try: await input_str.reply_to_message.delete()
+                except: pass
                 await input_str.delete()
             
             aio_list_btn = []
