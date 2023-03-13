66a05f25b24814bb77c7a1a2bf7158b25472ed39
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-13 10:08:48 +0530

    Update watermark45.py

--- plugins/dm/callBack/file_process/watermark45.py
+++ plugins/dm/callBack/file_process/watermark45.py
@@ -17,11 +17,11 @@ async def add_text_watermark(input_file, output_file, watermark_text):
                 font = fitz.Font(fontname="tiit")
                 text_width = font.text_length(watermark_text, fontsize=int(page.bound().height//20))
                 
-                tw = fitz.TextWriter(page.rect, opacity = .5, color = (0, 0, 0))
+                tw = fitz.TextWriter(page.rect, opacity = .5, color = (0, 0, 0), rotate = 45)
                 
                 txt_bottom, txt_left =  int((page.bound().width-text_width)/2), int((page.bound().height-page.bound().height/20)/2)
                 
-                tw.append((txt_bottom, txt_left), watermark_text, fontsize = int(page.bound().height//20), font = font, rotate = 45)
+                tw.append((txt_bottom, txt_left), watermark_text, fontsize = int(page.bound().height//20), font = font)
                 tw.write_text(page)
                 
             pdf.save(output_file)
