7eb89c46db6e530ded457b236978902567ab55e0
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-13 08:17:49 +0530

    Update __aio__.py

--- plugins/dm/callBack/__aio__.py
+++ plugins/dm/callBack/__aio__.py
@@ -77,12 +77,13 @@ async def __index__(bot, callbackQuery):
 #                 isSuccess, output_file = await previewPDF.previewPDF(input_file=input_file, cDIR=cDIR, editMessage=dlMSG, callbackQuery=callbackQuery)
             elif job == "compress" and work_info:
                 isSuccess, output_file = await compressPDF.compressPDF(input_file=input_file, cDIR=cDIR)
-            elif job == "text" and work_info:
-                isSuccess, output_file = await textPDF.textPDF(input_file=input_file, cDIR=cDIR, data=f"text{WORKS['text'][0].upper()}")
-                await callbackQuery.message.reply_document(
-                    file_name = output_file.split("/")[-1], quote = True, document = output_file,
-                    progress = render._progress, progress_args = (dlMSG, time.time()) 
-                )
+            elif job == "text":
+                if work_info:
+                    isSuccess, output_file = await textPDF.textPDF(input_file=input_file, cDIR=cDIR, data=f"text{WORKS['text'][0].upper()}")
+                    await callbackQuery.message.reply_document(
+                        file_name = output_file.split("/")[-1], quote = True, document = output_file,
+                        progress = render._progress, progress_args = (dlMSG, time.time()) 
+                    )
                 output_file = input_file
             elif job == "rotate" and work_info:
                 isSuccess, output_file = await rotatePDF.rotatePDF(input_file=input_file, angle=all_data[4].lower(), cDIR=cDIR)
