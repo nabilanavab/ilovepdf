f128dcb9255ed762675f6037a96d31f075b48489
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-13 09:10:46 +0530

    Update metadataPDF.py

--- plugins/dm/callBack/file_process/metadataPDF.py
+++ plugins/dm/callBack/file_process/metadataPDF.py
@@ -12,9 +12,10 @@ import fitz
 from plugins.utils  import *
 from pyrogram       import enums
 
-async def metadataPDF(input_file: str, cDIR: str) -> ( bool, str ):
+async def metadataPDF(input_file: str, cDIR: str, message) -> ( bool, str ):
     try:
         with fitz.open(input_file) as iNPUT:
+            await message.reply_chat_action(enums.ChatAction.TYPING)
             pdfMetaData = "".join(f"`{i} : {iNPUT.metadata[i]}`\n" for i in iNPUT.metadata if iNPUT.metadata[i] != "") if iNPUT.metadata else ""
             return (True, pdfMetaData) if pdfMetaData!="" else (False, "") 
         
