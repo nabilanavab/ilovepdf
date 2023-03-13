8844880038b0040ac2f5ab6b969aa0f449924c25
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-03-13 09:09:28 +0530

    Create metadataPDF.py

---
+++ plugins/dm/callBack/file_process/metadataPDF.py
@@ -0,0 +1,25 @@
+# fileName : plugins/dm/callBack/file_process/metadataPDF.py
+# copyright ©️ 2021 nabilanavab
+
+file_name = "plugins/dm/callBack/file_process/metadataPDF.py"
+__author_name__ = "Nabil A Navab: @nabilanavab"
+
+# LOGGING INFO: DEBUG
+from logger import logger
+
+
+import fitz
+from plugins.utils  import *
+from pyrogram       import enums
+
+async def metadataPDF(input_file: str, cDIR: str) -> ( bool, str ):
+    try:
+        with fitz.open(input_file) as iNPUT:
+            pdfMetaData = "".join(f"`{i} : {iNPUT.metadata[i]}`\n" for i in iNPUT.metadata if iNPUT.metadata[i] != "") if iNPUT.metadata else ""
+            return (True, pdfMetaData) if pdfMetaData!="" else (False, "") 
+        
+    except Exception as Error:
+        logger.exception("🐞 %s: %s" %(file_name, Error), exc_info = True)
+        return False, Error
+
+# Author: @nabilanavab
