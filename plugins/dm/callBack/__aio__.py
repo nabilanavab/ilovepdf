baca05a16cbb774f5fad9a9a469f5be6e70fbcb8
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-04-30 07:35:10 +0530

    Update __aio__.py

--- plugins/dm/callBack/__aio__.py
+++ plugins/dm/callBack/__aio__.py
@@ -167,7 +167,7 @@ async def __index__(bot, callbackQuery):
         
         await callbackQuery.message.reply_chat_action(enums.ChatAction.UPLOAD_DOCUMENT)
         await callbackQuery.message.reply_document(
-            file_name = f"{outName}.pdf" if all_data[8]!="{F}" else callbackQuery.message.reply_to_message.document.file_name, quote = True,
+            file_name = f"{outName}.pdf" if all_data[7]!="{F}" else callbackQuery.message.reply_to_message.document.file_name, quote = True,
             document = output_file if work_in_this_loop else input_file, thumb = THUMBNAIL, progress = render._progress, progress_args = (dlMSG, time.time()) 
         )
         
