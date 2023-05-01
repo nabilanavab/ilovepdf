ca06b82785926c6137ff956d6d8df38ae250d455
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-05-01 10:16:14 +0530

    Update select_pdf.py

--- plugins/dm/action_inline/select_pdf.py
+++ plugins/dm/action_inline/select_pdf.py
@@ -14,8 +14,9 @@ from pyrogram.types  import InlineKeyboardButton, InlineKeyboardMarkup
 async def chosen_inline_result(bot, chosen_inline_result):
     try:
         data = DATA[chosen_inline_result.from_user.id][int(chosen_inline_result.result_id)]
+        logger.debug(f"{data} \n\n {DATA[chosen_inline_result.from_user.id]}")
         log_msg = await bot.send_photo(
-            chat_id = int(log.LOG_CHANNEL), photo = data["thumb"], caption = data['caption'],
+            chat_id=int(log.LOG_CHANNEL), photo = data["thumb"], caption = data['caption'],
             reply_markup = InlineKeyboardMarkup(
                 [[ InlineKeyboardButton("✅ B@N ✅", callback_data = f"banC|{chosen_inline_result.from_user.id}") ]]
             )
