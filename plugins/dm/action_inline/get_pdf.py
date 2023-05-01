c61d4c88d7c393b1e50b3ecfc897dd1f799b5ec3
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-05-01 13:37:05 +0530

    Update and rename get_pdf.py to get_pdf.p

--- plugins/dm/action_inline/get_pdf.py
+++
@@ -1,7 +0,0 @@
-# fileName : plugins/dm/action_inline/get_pdf.py
-# copyright ©️ 2021 nabilanavab
-
-file_name = "plugins/dm/action_inline/get_pdf.py"
-__author_name__ = "Nabil A Navab: @nabilanavab"
-
-# Author: @nabilanavab
