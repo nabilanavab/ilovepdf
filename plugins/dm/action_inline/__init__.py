bfed3bc3dac62ebe627e04c88d1ee9a206d09822
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-05-01 08:34:13 +0530

    Update __init__.py

--- plugins/dm/action_inline/__init__.py
+++ plugins/dm/action_inline/__init__.py
@@ -4,4 +4,9 @@
 file_name = "plugins/dm/action_inline/__init__.py"
 __author_name__ = "Nabil A Navab: @nabilanavab"
 
+DATA = {
+    user_id : { '1' : 'MD5', '2' : 'MD5', '3' : 'MD5' },
+}
+
+
 # Author: @nabilanavab
