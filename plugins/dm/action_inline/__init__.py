8f47349269853d188dd6d8c2f61e607e3a700232
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-05-01 08:10:30 +0530

    Create __init__.py

---
+++ plugins/dm/action_inline/__init__.py
@@ -0,0 +1,7 @@
+# fileName : plugins/dm/action_inline/__init__.py
+# copyright ©️ 2021 nabilanavab
+
+file_name = "plugins/dm/action_inline/__init__.py"
+__author_name__ = "Nabil A Navab: @nabilanavab"
+
+# Author: @nabilanavab
