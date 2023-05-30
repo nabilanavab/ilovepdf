39e0e0c22506d441bb26b24c9d0f94bedcf01d57
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-05-30 20:39:11 +0530

    Update default.py

--- plugins/dm/action_inline/default.py
+++ plugins/dm/action_inline/default.py
@@ -11,11 +11,11 @@ from pyrogram.types  import (InputTextMessageContent, InlineKeyboardMarkup,
 
 async def default_ans(inline_query) -> list:
     try:
-        lang_code = await util.getLang(inline_query.from_user.id)
+        lang_code=await util.getLang(inline_query.from_user.id)
         CHUNK, _ = await util.translate(text="INLINE", lang_code=lang_code)
         
         # Getting Lang Data..
-        BUTTON=CHUNK['lang_b']
+        BUTTON=CHUNK['TOP']
         _lang = { langList[lang][1]:f"https://t.me/{myID[0].username}?start=#l{lang}#r{inline_query.from_user.id}" for lang in langList }
         BUTTON.update(_lang)
         BUTTON.update({"♻" : "-|refresh"})
@@ -30,32 +30,40 @@ async def default_ans(inline_query) -> list:
         answer = [
             InlineQueryResultPhoto(
                 photo_url="https://graph.org/file/e0543a5ace611768e71d1.jpg",
-                title=CHUNK['sear_t'], input_message_content=InputTextMessageContent(CHUNK['sear_d']),
+                title=CHUNK['sear_t'], description=CHUNK['sear_d'],
+                input_message_content=InputTextMessageContent(CHUNK['sear_d']),
                 reply_markup=InlineKeyboardMarkup(
                     [[ InlineKeyboardButton(text=CHUNK['search'], switch_inline_query_current_chat="" )]] ),
             ),
             InlineQueryResultPhoto(
                 photo_url="https://graph.org/file/4506c172bf757ce187fe1.jpg",
-                title=CHUNK['lang_t'], reply_markup=BUTTON, input_message_content=InputTextMessageContent(CHUNK['lang_d'])
-            ),
-            InlineQueryResultPhoto(
-                photo_url="https://graph.org/file/521e4aa2469427281bfac.jpg",
-                title=CHUNK['P2J_t'], reply_markup=openChat, input_message_content=InputTextMessageContent(CHUNK['P2J_d']),
-            ),
-            InlineQueryResultPhoto(
-                photo_url="https://graph.org/file/178e9f143814f695578fc.jpg",
-                title=CHUNK['lock_t'], reply_markup=openChat, input_message_content=InputTextMessageContent(CHUNK['lock_t']),
-            ),
-            InlineQueryResultPhoto(
-                photo_url="https://graph.org/file/e4baa95ecfd27b8dc1407.jpg",
-                title=CHUNK['save_t'], reply_markup=openChat, input_message_content=InputTextMessageContent(CHUNK['save_d']),
-            ),
-            InlineQueryResultPhoto(
-                photo_url="https://graph.org/file/521e4aa2469427281bfac.jpg",
-                title=CHUNK['P2J_t'], reply_markup=openChat, input_message_content=InputTextMessageContent(CHUNK['P2J_d']),
-            ),
+                title=CHUNK['lang_t'], reply_markup=BUTTON, description=CHUNK['lang_d'],
+                input_message_content=InputTextMessageContent(CHUNK['lang_d'])
+            )
         ]
         
         return answer
     except Exception as Error:
-        logger.exception("🐞 %s: %s" %(fileName, Error), exc_info = True)
+        logger.exception("🐞 %s: %s" %(fileName, Error), exc_info=True)
+
+async def search(inline_query) -> list:
+    try:
+        lang_code = await util.getLang(inline_query.from_user.id)
+        CHUNK, _ = await util.translate(text="INLINE", lang_code=lang_code)
+        
+        answer = [
+            InlineQueryResultPhoto(
+                photo_url="https://graph.org/file/e0543a5ace611768e71d1.jpg",
+                title=CHUNK['sear_t'], description=CHUNK['sear_d'],
+                input_message_content=InputTextMessageContent(CHUNK['sear_d']),
+                reply_markup=InlineKeyboardMarkup(
+                    [[ InlineKeyboardButton(
+                        text=CHUNK['search'],
+                        switch_inline_query_current_chat=f"{inline_query.qyery.split('|')[1]}")
+                    ]]
+                ),
+            )]
+        
+        return answer
+    except Exception as Error:
+        logger.exception("🐞 %s: %s" %(fileName, Error), exc_info=True)
