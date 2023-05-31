525929025c72947b3c948487d7a1ac3e58312679
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-05-31 14:42:56 +0530

    Update start.py

--- plugins/dm/start.py
+++ plugins/dm/start.py
@@ -48,8 +48,9 @@ async def start(bot, message):
         lang_code=await util.getLang(message.chat.id)
         
         if "+" in message.text:
-            lang_code, refer_id, get_pdf, md5_str = await extract_data(message.text)
-            
+            logger.debug("--------------------------")
+            lang_code, refer_id, get_pdf, md5_str = await extract_data(message.text+"+")
+            logger.debug(f"{get_pdf}")
             if lang_code and settings.MULTI_LANG_SUP and lang_code in langList:
                 userLang[chat_id] = lang_code
                 chat_type=message.chat.type
