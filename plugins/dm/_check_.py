b9604d99676e7a803d6ded6007c1f4dc77476355
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-01 07:55:53 +0530

    Update _check_.py

--- plugins/dm/_check_.py
+++ plugins/dm/_check_.py
@@ -17,13 +17,14 @@ from pyrogram.types       import Message
 from configs.db           import dataBASE
 from lang                 import langList
 from lang.__users__       import userLang
+from .start               import extract_data
 from pyrogram.errors      import UserNotParticipant
 from pyrogram             import Client as ILovePDF, filters
 
 if dataBASE.MONGODB_URI:
     from database import db
 
-# ==============================================================================================> BANNED USER <========================================================
+# ===============> BANNED USER <======================
 async def bannedUsers(_, __, message: Message):
     if (message.from_user.id in dm.BANNED_USERS) or (
             (dm.ADMIN_ONLY) and (message.from_user.id not in dm.ADMINS)) or (
@@ -31,6 +32,7 @@ async def bannedUsers(_, __, message: Message):
         return True
     return False
 banned_user=filters.create(bannedUsers)
+
 @ILovePDF.on_message(filters.private & banned_user & filters.incoming)
 async def bannedUsr(bot, message):
     try:
@@ -38,52 +40,54 @@ async def bannedUsr(bot, message):
         await message.reply_chat_action(enums.ChatAction.TYPING)
         # IF USER BANNED FROM DATABASE
         if message.from_user.id in BANNED_USR_DB:
-            ban = await db.get_key(id = message.from_user.id, key = "banned")
-            trans_txt, trans_btn = await util.translate(text = "BAN['UCantUseDB']", button = "BAN['banCB']", lang_code = lang_code)
+            ban = await db.get_key(id=message.from_user.id, key="banned")
+            trans_txt, trans_btn = await util.translate(text="BAN['UCantUseDB']", button="BAN['banCB']", lang_code=lang_code)
             return await message.reply_photo(
-               photo = images.BANNED_PIC, reply_markup = trans_btn, quote = True,
-               caption = trans_txt.format(message.from_user.mention, ban),
+               photo=images.BANNED_PIC, reply_markup=trans_btn, quote=True,
+               caption=trans_txt.format(message.from_user.mention, ban),
             )
         #IF USER BANNED FROM CONFIG.VAR
-        trans_txt, trans_btn = await util.translate(text = "BAN['UCantUse']", button = "BAN['banCB']", lang_code = lang_code)
+        trans_txt, trans_btn = await util.translate(text="BAN['UCantUse']", button="BAN['banCB']", lang_code=lang_code)
         return await message.reply_photo(
-            photo = images.BANNED_PIC, reply_markup = trans_btn, quote = True,
-            caption = trans_txt.format(message.from_user.mention),
+            photo=images.BANNED_PIC, reply_markup=trans_btn, quote=True,
+            caption=trans_txt.format(message.from_user.mention),
         )
     except Exception as e:
         logger.exception("🐞 %s /close: %s" %(file_name, e))
 
-# ============================================================> BANNED GROUP <=========================================================================================
+# ===================> BANNED GROUP <===================
 async def bannedGroups(_, __, message: Message):
     if (message.chat.id in group.BANNED_GROUP) or (
             (group.ADMIN_GROUP_ONLY) and (message.chat.id not in group.ADMIN_GROUPS)) or (
                 (dataBASE.MONGODB_URI) and (message.chat.id in BANNED_GRP_DB)):
         return True
     return False
+banned_group=filters.create(bannedGroups)
+
 async def setDb(_, bot, message: Message):
     if (dataBASE.MONGODB_URI) and (message.chat.id not in GROUPS):
         await log.newUser(bot, message, False, False)
         GROUPS.append(message.chat.id)
     return True
-banned_group=filters.create(bannedGroups)
 set_db=filters.create(setDb)
+
 @ILovePDF.on_message(filters.group & set_db & banned_group & filters.incoming)
 async def bannedGrp(bot, message):
     try:
         lang_code = await utils.getLang(message.chat.id)
         await message.reply_chat_action(enums.ChatAction.TYPING)
         if message.chat.id in BANNED_GRP_DB:
-            ban = await db.get_key(id = message.chat.id, key = "banned", typ = "group")
+            ban = await db.get_key(id=message.chat.id, key="banned", typ="group")
             trans_txt, trans_btn = await util.translate(text="BAN['GroupCantUseDB']", button="BAN['banCB']", lang_code=lang_code)
             toPin = await message.reply_photo(
-                photo = images.BANNED_PIC, reply_markup = trans_btn, quote = True,
-                caption = trans_txt.format(message.chat.title, ban)
+                photo=images.BANNED_PIC, reply_markup=trans_btn, quote=True,
+                caption=trans_txt.format(message.chat.title, ban)
             )
         else:
             trans_txt, trans_btn = await util.translate(text="BAN['GroupCantUse']", button="BAN['banCB']", lang_code=lang_code)
             toPin = await message.reply_photo(
-                photo = images.BANNED_PIC, reply_markup = trans_btn,
-                caption = trans_txt.format(message.chat.title), quote = True
+                photo=images.BANNED_PIC, reply_markup=trans_btn,
+                caption=trans_txt.format(message.chat.title), quote=True
             )
         try:
             await toPin.pin()
@@ -92,32 +96,23 @@ async def bannedGrp(bot, message):
     except Exception as e:
         logger.exception("🐞 %s /close: %s" %(file_name, e))
 
-# ====================================>  IF FORCE SUBSCRIPTION  <=====================================================================================================
+# =====================>  IF FORCE SUBSCRIPTION  <===================
 async def notSubscribed(_, bot, message: Message):
     if message.text and message.text.startswith("/start"):
         msg = message.text.split(" ")
-        if len(msg) != 1:
-            index = msg[1].find("-l")
-            lang = msg[1][index+2: index+5] if index != -1 else await util.getLang(message.chat.id)
-            if lang in langList:
-                userLang[message.chat.id] = lang
-            # https://t.me/bot?start=-l{lang_code}-r{user_id}   referID Optional
-            referID = msg[1].find("-r")
-            if referID != -1:    # -1 coz it return -1 if not founded
-                referID = msg[1][referID+2: ]
-        else:
-            referID = -1
-        lang_code = await util.getLang(message.chat.id)
+        if "-" in message.text:
+            lang_code, referID, get_pdf, md5_str = await extract_data(f"{message.text}-")
+            if lang_code and settings.MULTI_LANG_SUP and lang_code in langList:
+                userLang[message.chat.id]=lang
+        lang_code=await util.getLang(message.chat.id)
         if dataBASE.MONGODB_URI:               # CHECK IF USER IN DATABASE
             await log.newUser(bot, message, lang_code, int(referID))
         
-    if len(invite_link) == 0:
+    if len(invite_link)==0:
         return False                             # IF FORCE SUB. TREAT AS SUBSCRIBED
     else:
         try:
-            userStatus = await bot.get_chat_member(
-                str(settings.UPDATE_CHANNEL), message.from_user.id
-            )
+            userStatus = await bot.get_chat_member(str(settings.UPDATE_CHANNEL), message.from_user.id)
             if userStatus.status == 'kicked':    # IF USER BANNED FROM CHANNEL
                 return True
             return False
@@ -126,6 +121,7 @@ async def notSubscribed(_, bot, message: Message):
         except Exception as e:
             return True
 not_subscribed = filters.create(notSubscribed)
+
 @ILovePDF.on_message(filters.private & filters.incoming & not_subscribed)
 async def non_subscriber(bot, message):
     try:
