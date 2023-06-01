969dc4dbf722c3ced41775d34941c83eb7e200d8
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-01 14:37:37 +0530

    Update in_bot.py

--- plugins/dm/action_inline/in_bot.py
+++ plugins/dm/action_inline/in_bot.py
@@ -37,7 +37,7 @@ async def openInBot( bot, message, message_id: int ) -> bool:
         trCHUNK, _ = await translate(text="INLINE", lang_code=lang_code)
         
         getMSG = await bot.get_messages(chat_id=int(log.LOG_CHANNEL), message_ids=int(message_id))
-        logger.debug(getMSG.media)
+        logger.debug(getMSG.media!=MessageMediaType.photo)
         if getMSG.empty or getMSG.media!=MessageMediaType.photo:
             return await callbackQuery.answer(trCHUNK['old'])
         
