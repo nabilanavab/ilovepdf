8cb611784f0efad774ab0b320b79b8a28bd5ef99
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-01 10:09:14 +0530

    Create in_bot.py

---
+++ plugins/dm/action_inline/in_bot.py
@@ -0,0 +1,76 @@
+# fileName : plugins/dm/action_inline/in_bot.py
+# copyright ©️ 2021 nabilanavab
+
+fileName = "plugins/dm/action_inline/in_bot.py"
+__author_name__ = "Nabil A Navab: @nabilanavab"
+
+from plugins.utils        import *
+from configs.log          import log
+from plugins.utils.work   import work
+from libgenesis           import Libgen
+from plugins.utils.util   import getLang, translate
+
+async def download(current, total, message, _type):
+    try:
+        await bot.edit_inline_reply_markup(
+            inline_message_id = message,
+            reply_markup = InlineKeyboardMarkup(
+                [[ 
+                    InlineKeyboardButton("📥 {} {:.2f}% 📥".format(_type, current/total*100), callback_data="https://t.me/ilovepdf_bot")
+                ]] 
+            ))
+    except errors.MessageNotModified as e:
+        logger.debug("🐞 %s: %s" %(fileName, e))
+    except errors.FloodWait as e:
+        logger.debug("🐞 %s: %s" %(fileName, e))
+        await asyncio.sleep(e.x)
+    except Exception as e:
+        logger.debug("🐞 %s: %s" %(fileName, e))
+
+async def openInBot( bot, message, message_id: int ) -> bool:
+    try:
+        lang_code=await getLang(user_id)
+        trCHUNK, _ = await translate(text="INLINE", lang_code=lang_code)
+        
+        getMSG = await bot.get_messages(chat_id=int(log.LOG_CHANNEL), message_ids=int(message_id))
+        if getMSG.empty or not getMSG.photo:
+            return await message.reply("❌")
+        
+        if await work(message, "check", True):
+            return await message.answer(trCHUNK['inWork'])
+        cDIR = await work(message, "create", True)
+        
+        photo=await bot.copy_message(
+            chat_id=message.chat.id, from_chat_id=int(log.LOG_CHANNEL),
+            message_id=int(message_id), reply_to_message_id=message.id,
+            reply_markup = InlineKeyboardMarkup(
+                [[ InlineKeyboardButton("⬇ STARTED DOWNLOADING ⬇", url="https://t.me/ilovepdf_bot")]]
+            )
+        )
+        
+        caption=getMSG.caption
+        md5=caption.splitlines()[0].split(':')[1].strip()
+        link=f'http://library.lol/main/{md5}'
+        
+        file = await Libgen().download(
+            link, dest_folder=cDIR, progress=cbPRO, progress_args=[photo.id, "DOWNLOADED"]
+        )
+        
+        await bot.edit_inline_reply_markup(
+            inline_message_id = callbackQuery.inline_message_id,
+            reply_markup = InlineKeyboardMarkup(
+                [[ InlineKeyboardButton("🐍 STARTED UPLOADING 🐍", callback_data=f"{callbackQuery.data}")],[
+                    InlineKeyboardButton("🗑️ CANCEL 🗑️", callback_data=f"c{callbackQuery.data[1:]}")]]
+            )
+        )
+        
+        await callbackQuery.message.reply_document(
+            document=file, caption=caption, progress=render.cbPRO,
+            progress_args = (callbackQuery.message, 0, "UPLOADED", True)
+        )
+        
+        return await work(message, "delete", True)
+    
+    except Exception as e:
+        logger.debug("🐞 %s: %s" %(fileName, e))
+      
