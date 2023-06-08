aa7f6a6f4e66f8775e3f2c0d6690827279a4c1f8
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-08 10:49:16 +0530

    Update default.py

--- plugins/dm/action_inline/default.py
+++ plugins/dm/action_inline/default.py
@@ -38,7 +38,7 @@ async def default_ans(inline_query) -> list:
             ),
             InlineQueryResultPhoto(
                 photo_url="https://graph.org/file/f47963d0d7a74d280bae1.jpg",
-                title=CHUNK['refer_t'], description=CHUNK['refer_d'],
+                title=CHUNK['refer_t'], description=CHUNK['refer_d'], disable_web_page_preview=True,
                 input_message_content=InputTextMessageContent(f"[@{myID[0].username}](https://t.me/{myID[0].username}?start=-r{inline_query.from_user.id})")
             )
         ]
