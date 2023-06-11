4714ee4348473ec7ba027bafb1825b2b6a89a3d8
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-12 03:35:04 +0530

    Update __init__.py

--- plugins/dm/textToPdf/__init__.py
+++ plugins/dm/textToPdf/__init__.py
@@ -1,5 +1,16 @@
+file_name = "plugins/dm/textToPdf/__init__.py"
+author_name = "telegram.dog/nabilanavab"
+source_code = "https://github.com/nabilanavab/ilovepdf"
+
+# GLOBAL VARIABLES
+
+# Just a DB Variable
+TXT = {}
+
 FONT = {"t": "Times", "c": "Courier", "h": "Helvetica", "s": "Symbol", "z": "ZapfDingbats"}
 
 COLOR = {}
 
 PAGE_SIZE = {}
+
+# UPDATE CHANNEL: https://telegram.dog/ilovepdf_bot
