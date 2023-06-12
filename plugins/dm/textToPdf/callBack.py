3ed1a58f0ce390d93b67c1bb4477114c90c4cedd
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-13 02:44:01 +0530

    Update callBack.py

--- plugins/dm/textToPdf/callBack.py
+++ plugins/dm/textToPdf/callBack.py
@@ -7,9 +7,10 @@ from plugins.utils       import *
 from configs.log         import log
 from fpdf                import FPDF
 from logger              import logger
+from pyrogram.types      import InputMediaPhoto
 from configs.config      import settings, images
 from .                   import FONT, COLOR, PAGE_SIZE
-from pyrogram            import filters, Client as ILovePDF, enums
+from pyrogram            import filters, Client as ILovePDF
 
 @ILovePDF.on_callback_query(filters.regex("^t2p"))
 async def text_to_pdf_cb(bot, callbackQuery):
@@ -27,7 +28,7 @@ async def text_to_pdf_cb(bot, callbackQuery):
             tTXT = await util.editDICT(inDir=tTXT, value=f"{data[1]}")
             tTXT = await util.createBUTTON(tTXT, "12121")
             return await callbackQuery.edit_message_media(
-                media= "https://graph.org/file/c301b7af1e637f642a520.jpg",reply_markup=tTXT
+                media=InputMediaPhoto("https://graph.org/file/c301b7af1e637f642a520.jpg"), reply_markup=tTXT
             )
         
         elif len(data) == 3:
