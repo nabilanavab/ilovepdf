a8f362e3bd7ab4003d796666437bcbd89f7f77ba
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-14 03:10:47 +0530

    Update __init__.py

--- plugins/dm/textToPdf/__init__.py
+++ plugins/dm/textToPdf/__init__.py
@@ -7,15 +7,27 @@ source_code = "https://github.com/nabilanavab/ilovepdf"
 # Just a DB Variable
 TXT = {}
 
-FONT = {
-        1 : "Times",
-        2 : "Courier",
-        3 : "Helvetica",
-        4 : "Symbol",
-        5 : "ZapfDingbats"
+SCALE = {
+        1 : "Landscape",
+        2 : "Portrait"
     }
 
-COLOR = {}
+FONT = {
+        1 : { "default" : True, "name" : "Times" },
+        2 : { "default" : True, "name" : "Courier" },
+        3 : { "default" : True, "name" : "Helvetica" },
+        4 : { "default" : True, "name" : "Symbol" },
+        5 : { "default" : True, "name" : "ZapfDingbats" }
+    }
+
+COLOR = {
+        1 : { "color" : "white", "code" : (255, 255, 255) },
+        2 : { "color" : "black", "code" : (0, 0, 0) },
+        3 : { "color" : "grey", "code" : (128, 128, 128) },
+        4 : { "color" : "red", "code" : (255, 0, 0) },
+        5 : { "color" : "green", "code" : (0, 255, 0) },
+        6 : { "color" : "blue", "code" : (0, 0, 255) }
+}
 
 PAGE_SIZE = {}
 
