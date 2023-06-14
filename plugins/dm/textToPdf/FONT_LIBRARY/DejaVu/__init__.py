998ee645fa4e7bf3f5907360bc2aeacfc46cd943
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-14 08:22:38 +0530

    Create __init__.py

---
+++ plugins/dm/textToPdf/FONT_LIBRARY/DejaVu/__init__.py
@@ -0,0 +1,9 @@
+
+import os
+
+async def add_DejaVu(pdf)
+    for file_name in os.listdir('./'):
+        if file_name.endswith('.ttf'):
+            font_name = os.path.splitext(file_name)[0]
+            font_path = os.path.join(font_directory, file_name)
+            pdf.add_font(font_name, '', font_path, uni=True)
