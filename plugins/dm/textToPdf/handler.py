993db0e6731db627a1e159debfe082924f04b145
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-15 02:43:49 +0530

    Update handler.py

--- plugins/dm/textToPdf/handler.py
+++ plugins/dm/textToPdf/handler.py
@@ -90,7 +90,6 @@ async def text_to_pdf(bot, callbackQuery):
             pdf.set_font('headFont', '', size=20)
         else:
             pdf = await add_DejaVu(pdf)
-            logger.debug(dict(pdf))
             pdf.set_font("DejaVu", "", size=20)
         
         if TXT[callbackQuery.message.chat.id][0] != None:
@@ -101,7 +100,6 @@ async def text_to_pdf(bot, callbackQuery):
             pdf.set_font('paraFont', '', size=20)
         else:
             pdf = await add_DejaVu(pdf)
-            logger.debug(pdf)
             pdf.set_font("DejaVu", "", size=20)
         
         for _ in TXT[callbackQuery.message.chat.id][1:]:
