c53bdecde0fd3eb672d86de7a4254bcb9a026909
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-16 04:54:25 +0530

    Update handler.py

--- plugins/dm/textToPdf/handler.py
+++ plugins/dm/textToPdf/handler.py
@@ -35,8 +35,8 @@ async def ask_for_bg(bot, callbackQuery, text: str):
                           reply_markup = ForceReply(True))
         
     if askBG.photo:
-        logger.debug(askBG.photo)
-        return askBG.photo
+        logger.debug(askBG.photo.file_id)
+        return askBG.photo.file_id
     else:
         return 1
 
