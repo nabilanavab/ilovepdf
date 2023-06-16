ee8a7e90b108dada38cd64a313d58552f616c7e5
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-16 05:50:27 +0530

    Update handler.py

--- plugins/dm/textToPdf/handler.py
+++ plugins/dm/textToPdf/handler.py
@@ -42,8 +42,9 @@ async def ask_for_paragraph(bot, callbackQuery, text: str, num: int = False):
                 return True, None
             return True, askTEXT
         elif askTEXT.photo:
+            logger.debug(askTEXT)
             return True, { 'type': 'photo', 'id': askTEXT.photo.file_id,
-                          'caption': askTEXT.photo.caption if askTEXT.photo.caption else None}
+                          'caption': askTEXT.caption if askTEXT.caption else None}
 
 async def ask_for_bg(bot, callbackQuery, text: str):
     askBG = await bot.ask(text=text, chat_id=callbackQuery.message.chat.id,
