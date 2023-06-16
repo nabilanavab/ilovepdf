0065fa4794bbb894bacc3d0823a689dbe0295fda
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-06-17 01:02:14 +0530

    Update handler.py

--- plugins/dm/textToPdf/handler.py
+++ plugins/dm/textToPdf/handler.py
@@ -124,10 +124,10 @@ async def text_to_pdf(bot, callbackQuery):
         elif scale == 2 and BACKGROUND_P.get(background, False):
             POSITION = BACKGROUND_P[background]['position']
         else:
-            POSITION = [200, 20, 200, 10]
+            POSITION = [20, 10]
         
         if TXT[callbackQuery.message.chat.id][0] != None:
-            pdf.cell(POSITION[0], POSITION[1], txt=get_display(reshape(TXT[callbackQuery.message.chat.id][0])), ln=True, align="C")
+            pdf.cell(pdf.w, 20, txt=get_display(reshape(TXT[callbackQuery.message.chat.id][0])), ln=True, align="C")
         
         pdf.add_font('paraFont', '', FONT[p_font], uni=True)
         pdf.set_font('paraFont', '', size=20)
@@ -135,7 +135,7 @@ async def text_to_pdf(bot, callbackQuery):
         for para in TXT[callbackQuery.message.chat.id][1:]:
             # pdf.set_x(10)
             if isinstance(para, str):
-                pdf.multi_cell(POSITION[2], POSITION[3], txt=get_display(reshape(f"     {para}")), ln=True, align="L")
+                pdf.multi_cell(pdf.w, 10, txt=get_display(reshape(f"     {para}")), ln=True, align="L")
             if isinstance(para, dict):
                 if para['type']=='photo':
                     img = await bot.download_media(message=para['id'], file_name=f"{cDIR}/")
