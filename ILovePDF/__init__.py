61d22c72868e8c8225e7b2d178e8a7140f299daa
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-07-05 03:50:12 +0530

    Create __init__.py

---
+++ ILovePDF/__init__.py
@@ -0,0 +1,16 @@
+# This file is part of nabilanavab/iLovePDF [a completely free software]
+
+__author__ = "nabilanavab"
+__email__ = "nabilanavab@gmail.com"
+__telegram__ = "telegram.dog/nabilanavab"
+__copyright__ = "Copyright 2021, nabilanavab"
+
+iLovePDF = '''
+  _   _                  ___  ___  ____ ™
+ | | | |   _____ _____  | _ \|   \|  __| 
+ | | | |__/ _ \ V / -_) |  _/| |) |  _|  
+ |_| |___,\___/\_/\___| |_|  |___/|_|    
+                         ❤ [Nabil A Navab] 
+                         ❤ Email: nabilanavab@gmail.com
+                         ❤ Telegram: @nabilanavab
+'''
