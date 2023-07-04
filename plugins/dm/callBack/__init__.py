a5507d07ba57ecd970397a45c4e7787edcbaf501
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-07-05 03:43:13 +0530

    Delete plugins directory

--- plugins/dm/callBack/__init__.py
+++
@@ -1,7 +0,0 @@
-# fileName : plugins/dm/callBack/__init__.py
-# copyright ©️ 2021 nabilanavab
-
-file_name = "plugins/dm/callBack/__init__.py"
-__author_name__ = "Nabil A Navab: @nabilanavab"
-
-# Author: @nabilanavab
--- plugins/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from . import work, render, fncta, util, caption
-
-__all__ = ["work", "render", "fncta", "util", "caption"]
