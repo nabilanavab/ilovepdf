15deff39384e2374908505c1ddb426badf188e0b
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-07-05 03:42:53 +0530

    Delete libgenesis directory

--- libgenesis/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-from .search import Libgen
-
-__version__ = '0.1.6'
-logging.basicConfig(
-    level=logging.CRITICAL,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-logging.getLogger(__name__)
