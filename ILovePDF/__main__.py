b4889f3c8566a976d5ae256fbcb7ad8297121812
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-07-06 03:52:18 +0530

    Update __main__.py

--- ILovePDF/__main__.py
+++ ILovePDF/__main__.py
@@ -2,7 +2,7 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # copyright ©️ 2021 nabilanavab
 
-from . import *
+from __init__ import *
 file_name = os.path.abspath(__file__)
 
 from configs.db import *
