aad11d7e2557d791550dc1f1dac0249a8a5f5996
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-07-06 04:21:13 +0530

    Update setup.py

--- ILovePDF/setup.py
+++ ILovePDF/setup.py
@@ -2,7 +2,7 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # copyright ©️ 2021 nabilanavab
 
-file_name = os.path.abspath(__file__)
+file_name = "ILovePDF/setup.py"
 
 from setuptools import setup, find_packages
 
