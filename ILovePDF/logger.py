ae74c0a8cc295d69eee2d8a0acea940c34d3ae4e
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-07-06 04:20:37 +0530

    Update logger.py

--- ILovePDF/logger.py
+++ ILovePDF/logger.py
@@ -2,7 +2,7 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # copyright ©️ 2021 nabilanavab
 
-file_name = os.path.abspath(__file__)
+file_name = "ILovePDF/logger.py"
 
 import logging
 logger = logging.getLogger(__name__)
