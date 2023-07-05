5a6f3de997d36b4b50cbd20243c425365c700aad
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-07-05 12:34:21 +0530

    Create waste.py

---
+++ ILovePDF/plugins/dm/waste.py
@@ -0,0 +1,23 @@
+# This module is part of https://github.com/nabilanavab/ilovepdf
+# Feel free to use and contribute to this project. Your contributions are welcome!
+# copyright ©️ 2021 nabilanavab
+
+file_name = os.path.abspath(__file__)
+
+from plugins.utils import *
+from configs.config import dm
+from pyrogram import Client as ILovePDF, enums, filters
+
+# WASTE/DPAMMING MESSAGES 
+@ILovePDF.on_message(filters.private & filters.incoming & ~filters.user(dm.ADMINS))
+async def _spam(bot, message):
+    try:
+        lang_code = await util.getLang(message.chat.id)
+        await message.reply_chat_action(enums.ChatAction.TYPING)
+        tTXT, tBTN = await util.translate(text="noHelp", lang_code=lang_code)
+        await message.reply_text(tTXT, quote=True)
+    except Exception as e:
+        logger.exception("🐞 %s: %s" % (file_name, e), exc_info=True)
+
+# If you have any questions or suggestions, please feel free to reach out.
+# Together, we can make this project even better, Happy coding!  XD
