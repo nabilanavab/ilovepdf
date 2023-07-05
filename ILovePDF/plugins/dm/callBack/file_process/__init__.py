7aa550a253e07eefbf4e25278eeb92ba36e2c2a7
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-07-06 01:25:43 +0530

    Create __init__.py

---
+++ ILovePDF/plugins/dm/callBack/file_process/__init__.py
@@ -0,0 +1,59 @@
+# This module is part of https://github.com/nabilanavab/ilovepdf
+# Feel free to use and contribute to this project. Your contributions are welcome!
+# copyright ©️ 2021 nabilanavab
+
+file_name = os.path.abspath(__file__)
+
+__author__ = "nabilanavab"
+__email__ = "nabilanavab@gmail.com"
+__telegram__ = "telegram.dog/nabilanavab"
+__copyright__ = "Copyright 2021, nabilanavab"
+
+iLovePDF = '''
+  _   _                  ___  ___  ____ ™
+ | | | |   _____ _____  | _ \|   \|  __| 
+ | | | |__/ _ \ V / -_) |  _/| |) |  _|  
+ |_| |___,\___/\_/\___| |_|  |___/|_|    
+                         ❤ [Nabil A Navab] 
+                         ❤ Email: nabilanavab@gmail.com
+                         ❤ Telegram: @nabilanavab
+'''
+
+from . import *
+
+__all__ = [
+    "previewPDF",
+    "textPDF",
+    "zoomPDF",
+    "formatPDF",
+    "encryptPDF",
+    "decryptPDF",
+    "combinePages",
+    "blackAndWhitePdf",
+    "compressPDF",
+    "decryptPDF",
+    "drawPDF",
+    "ocrPDF",
+    "rotatePDF",
+    "saturatePDF",
+    "pdfToImages",
+    "renamePDF",
+    "splitPDF",
+    "mergePDF",
+    "stampPDF",
+    "twoPagesToOne",
+    "threePagesToOne",
+    "threePagesToOneH",
+    "twoPagesToOneH",
+    "watermarkPDF",
+    "deletePDFPg",
+    "zipTarPDF",
+    "messagePDF",
+    "metadataPDF",
+    "watermark45",
+    "invertPDF",
+]
+
+
+# If you have any questions or suggestions, please feel free to reach out.
+# Together, we can make this project even better, Happy coding!  XD
