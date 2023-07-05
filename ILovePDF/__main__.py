0d2fda50a2ab59720b73a7059ebe43b3f8875bd9
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-07-05 10:25:11 +0530

    Update __main__.py

--- ILovePDF/__main__.py
+++ ILovePDF/__main__.py
@@ -1,5 +1,6 @@
 # This module is part of https://github.com/nabilanavab/ilovepdf
 # Feel free to use and contribute to this project. Your contributions are welcome!
+# copyright ©️ 2021 nabilanavab
 
 file_name = os.path.abspath(__file__)
 
