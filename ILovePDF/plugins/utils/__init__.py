a9eb93f6a76a141e582d5c742356153df5590dfc
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-07-05 10:24:37 +0530

    Create __init__.py

---
+++ ILovePDF/plugins/utils/__init__.py
@@ -0,0 +1,25 @@
+# This module is part of https://github.com/nabilanavab/ilovepdf
+# Feel free to use and contribute to this project. Your contributions are welcome!
+# copyright ©️ 2021 nabilanavab
+
+__author__ = "nabilanavab"
+__email__ = "nabilanavab@gmail.com"
+__telegram__ = "telegram.dog/nabilanavab"
+__copyright__ = "Copyright 2021, nabilanavab"
+
+iLovePDF = '''
+  _   _                  ___  ___  ____ ™
+ | | | |   _____ _____  | _ \|   \|  __| 
+ | | | |__/ _ \ V / -_) |  _/| |) |  _|  
+ |_| |___,\___/\_/\___| |_|  |___/|_|    
+                         ❤ [Nabil A Navab] 
+                         ❤ Email: nabilanavab@gmail.com
+                         ❤ Telegram: @nabilanavab
+'''
+
+from . import work, render, fncta, util, caption
+
+__all__ = ["work", "render", "fncta", "util", "caption"]
+
+# If you have any questions or suggestions, please feel free to reach out.
+# Together, we can make this project even better, Happy coding!  XD
