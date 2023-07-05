fac65c94b90cb2fec66ff3a45b0b891a842aa8cc
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-07-06 04:23:03 +0530

    Update config.py

--- ILovePDF/configs/config.py
+++ ILovePDF/configs/config.py
@@ -2,8 +2,9 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # copyright ©️ 2021 nabilanavab
 
-file_name = os.path.abspath(__file__)
+file_name = "ILovePDF/configs/config.py"
 
+import os
 
 class bot(object):
 
