b597efd2a690ac485e068395a90cf3bf73ad78a9
Nabil A Navab <nabilanavab+telegram@gmail.com>
2023-07-07 03:35:17 +0530

    updated..

--- ILovePDF/web/domain.py
+++ ILovePDF/web/domain.py
@@ -4,18 +4,7 @@ import random
 app = Flask(__name__)
 
 # List of quotes
-quotes = [
-    "Straight to the point, no time for hellos.",
-    "Chat time is precious, skip the hellos.",
-    "Greetings are nice, but let's dive into the chat.",
-    "No time for hellos, let's get down to business.",
-    "Skip the pleasantries, let's chat efficiently.",
-    "In this chat, hellos can take a backseat.",
-    "Save time, skip the hellos, and dive in.",
-    "Hello? Let's cut to the chase and start the chat.",
-    "Quick chats, no time for greetings.",
-    "Efficiency is key, no time for hellos."
-]
+from quotes import quotes
 
 @app.route('/')
 def get_random_quote():
