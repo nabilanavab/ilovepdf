b952476396e32cb6a27a0186df9acf029ecf76d5
Nabil A Navab <nabilanavab+telegram@gmail.com>
2023-07-07 01:35:31 +0530

    updated link

--- ILovePDF/plugins/dm/callBack/file_process/link.py
+++ ILovePDF/plugins/dm/callBack/file_process/link.py
@@ -2,7 +2,7 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # copyright ©️ 2021 nabilanavab
 
-file_name = os.path.abspath(__file__)
+file_name = "ILovePDF/plugins/dm/callBack/file_process/link.py"
 
 import base64
 from asyncio import sleep
