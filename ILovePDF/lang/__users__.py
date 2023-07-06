dcfe0a4b4a57d5a993a75be01809cc3cf3a08bf4
✰Naͥbiͣlͫ A Navab✰ <53673312+nabilanavab@users.noreply.github.com>
2023-07-06 11:08:54 +0530

    Create __users__.py

---
+++ ILovePDF/lang/__users__.py
@@ -0,0 +1,15 @@
+# This module is part of https://github.com/nabilanavab/ilovepdf
+# Feel free to use and contribute to this project. Your contributions are welcome!
+# copyright ©️ 2021 nabilanavab
+
+file_name = "ILovePDF/lang/__users__.py"
+
+# Default User Lang Data:
+
+# It works even without Database [mongo]
+# Syntax := UserID: langCode
+
+userLang = {
+  # 531733867: "eng",
+  # 5161864280: "mal"
+}
