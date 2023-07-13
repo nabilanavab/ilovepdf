18f23b1d0408d260a075dcc2da82f937a0f24453
nabilanavab <nabilanavab+telegram@gmail.com>
2023-07-13 11:26:19 +0530

    debugging: error

--- ILovePDF/plugins/dm/callBack/file_process/partPDF.py
+++ ILovePDF/plugins/dm/callBack/file_process/partPDF.py
@@ -7,6 +7,7 @@ file_name = "ILovePDF/plugins/dm/callBack/file_process/partPDF.py"
 import os
 from logger import logger
 from pyrogram import filters
+from pyrogram.types import ForceReply
 from PyPDF2 import PdfWriter, PdfReader
 
 
