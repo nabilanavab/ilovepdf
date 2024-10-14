ea4328b02950c669d0a902d230cb31ee973ecb02
Nabil A Navab <nabilanavab@gmail.com>
2024-10-14 22:43:33 +0530

    annotation test

--- ILovePDF/database.py
+++ ILovePDF/database.py
@@ -2,7 +2,7 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # copyright ©️ 2021 nabilanavab
 
-file_name = "ILovePDF/database.py"
+file_name: str = "ILovePDF/database.py"
 
 # pip install motor
 import datetime
