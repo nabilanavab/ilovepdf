3168fc24e31106a0c74dde263e0340e538332444
Nabil A Navab <nabilanavab@gmail.com>
2024-10-15 23:35:43 +0530

    Autumn cleaned the code..

--- ILovePDF/plugins/dm/__check__.py
+++ ILovePDF/plugins/dm/__check__.py
@@ -2,7 +2,8 @@
 # Feel free to use and contribute to this project. Your contributions are welcome!
 # copyright ©️ 2021 nabilanavab
 
-file_name = "ILovePDF/plugins/dm/__check__.py"
+
+file_name: str = "ILovePDF/plugins/dm/__check__.py"
 
 from plugins import *
 from configs.db import *
@@ -22,7 +23,18 @@ if dataBASE.MONGODB_URI:
     from database import db
 
 
-async def stopBot(_, __, message: Message):
+async def stopBot(_, __, message: Message) -> bool:
+    """
+    Checks if the bot should be stopped based on the message and user permissions.
+
+    Parameters:
+    - _, __ : Ignored parameter
+    - message: The message object containing chat and user information.
+
+    Returns:
+    - False if the bot should stop and the user is an admin sending the stop command.
+    - True if the bot is configured to stop (based on settings), otherwise False.
+    """
     if (message.chat.id in dm.ADMINS) and message.text == "/stop":
         return False
     return True if settings.STOP_BOT else False
@@ -30,31 +42,68 @@ async def stopBot(_, __, message: Message):
 
 _stop_bot = filters.create(stopBot)
 @ILovePDF.on_message(_stop_bot & filters.incoming)
-async def stop_bot(bot, message):
+async def stop_bot(bot: ILovePDF, message: Message):
+    """
+    Handles the /stop command to respond to the user with a confirmation message.
+
+    Parameters:
+    - bot: The ILovePDF bot instance.
+    - message: The incoming message object containing chat and user information.
+
+    This function performs the following actions:
+    - Checks if the user is in the database and extracts related data.
+    - Translates the stop message and prepares a response.
+    - Sends a welcome photo with the translated text and buttons.
+
+    Returns:
+    - Sends a reply photo with the translated stop message and buttons.
+    """
     try:
         lang_code = await util.getLang(message.chat.id)
         await message.reply_chat_action(enums.ChatAction.TYPING)
-        if dataBASE.MONGODB_URI:  # CHECK IF USER IN DATABASE
+
+        # CHECK IF USER IN DATABASE
+        if dataBASE.MONGODB_URI:
             lang_code, referID, get_pdf, md5_str = await extract_data(
                 f"{message.text}-"
             )
             await log.newUser(bot, message, lang_code, referID)
+
         trans_txt, trans_btn = await util.translate(
-            text="_STOP", button="_STOP_CB", lang_code=lang_code
+            text = "_STOP", button = "_STOP_CB", lang_code = lang_code
         )
+
+        # Reply with a welcome photo and translated text
         return await message.reply_photo(
-            photo=images.WELCOME_PIC,
-            reply_markup=trans_btn,
-            caption=trans_txt.format(
-                message.from_user.mention, myID[0].mention
+            photo = images.WELCOME_PIC,
+            reply_markup = trans_btn,
+            caption = trans_txt.format(
+                message.from_user.mention,
+                myID[0].mention
             )
         )
+    
     except Exception as Error:
         logger.exception("🐞 %s stop_bot: %s" % (file_name, Error))
 
 
 # BANNED USER
-async def bannedUsers(_, __, message: Message):
+async def bannedUsers(_, __, message: Message) -> bool:
+    """
+    Checks if a user is banned from using the bot.
+
+    Parameters:
+    - _, __ : Unused parameter
+    - message: The incoming message object containing user information.
+
+    This function performs the following checks:
+    - Verifies if the user ID is in the list of banned users.
+    - Checks if the bot is restricted to admins only and if the user is not an admin.
+    - Verifies if the user ID is in the banned user database, if applicable.
+
+    Returns:
+    - True if the user is banned or not allowed to use the bot; otherwise, returns False.
+    """
     if (
         (message.from_user.id in dm.BANNED_USERS)
         or ((dm.ADMIN_ONLY) and (message.from_user.id not in dm.ADMINS))
@@ -66,38 +115,63 @@ async def bannedUsers(_, __, message: Message):
 
 banned_user = filters.create(bannedUsers)
 @ILovePDF.on_message(filters.private & banned_user & filters.incoming)
-async def bannedUsr(bot, message):
+async def bannedUsr(bot: ILovePDF, message: Message):
+    """
+    Handles messages from banned users in private chats.
+    This function checks if a user is banned
+
+    Parameters:
+    - bot: The instance of the bot handling the message.
+    - message: The incoming message object containing user information.
+    """
     try:
-        lang_code = await util.getLang(message.chat.id)
+        lang_code: str = await util.getLang(message.chat.id)
         await message.reply_chat_action(enums.ChatAction.TYPING)
+        
         # IF USER BANNED FROM DATABASE
         if message.from_user.id in BANNED_USR_DB:
-            ban = await db.get_key(id=message.from_user.id, key="banned")
+
+            ban = await db.get_key(id = message.from_user.id, key = "banned")
             trans_txt, trans_btn = await util.translate(
-                text="BAN['UCantUseDB']", button="BAN['banCB']", lang_code=lang_code
+                text = "BAN['UCantUseDB']", button = "BAN['banCB']", lang_code = lang_code
             )
+
             return await message.reply_photo(
-                photo=images.BANNED_PIC,
-                reply_markup=trans_btn,
-                quote=True,
-                caption=trans_txt.format(message.from_user.mention, ban),
+                photo = images.BANNED_PIC,
+                reply_markup = trans_btn, quote = True,
+                caption = trans_txt.format(message.from_user.mention, ban),
             )
+        
         # IF USER BANNED FROM CONFIG.VAR
         trans_txt, trans_btn = await util.translate(
-            text="BAN['UCantUse']", button="BAN['banCB']", lang_code=lang_code
+            text = "BAN['UCantUse']", button = "BAN['banCB']", lang_code = lang_code
         )
+
         return await message.reply_photo(
-            photo=images.BANNED_PIC,
-            reply_markup=trans_btn,
-            quote=True,
-            caption=trans_txt.format(message.from_user.mention),
+            photo = images.BANNED_PIC,
+            reply_markup = trans_btn, quote = True,
+            caption = trans_txt.format(message.from_user.mention),
         )
+    
     except Exception as Error:
         logger.exception("🐞 %s bannedUsr: %s" % (file_name, Error))
 
 
-#  BANNED GROUP 
-async def bannedGroups(_, __, message: Message):
+# BANNED GROUP 
+async def bannedGroups(_, __, message: Message) -> bool:
+    """
+    Checks if a group is banned from using the bot.
+
+    This function evaluates whether the incoming message originates from a banned group 
+    or an admin-only group that does not have the necessary permissions.
+
+    Parameters:
+    - _, __ : Unused parameter
+    - message: The incoming message object containing group information.
+
+    Returns:
+    - True if the group is banned, False otherwise.
+    """
     if (
         (message.chat.id in group.BANNED_GROUP)
         or ((group.ADMIN_GROUP_ONLY) and (message.chat.id not in group.ADMIN_GROUPS))
@@ -108,53 +182,63 @@ async def bannedGroups(_, __, message: Message):
 
 
 banned_group = filters.create(bannedGroups)
-async def setDb(_, bot, message: Message):
+async def setDb(_, bot: ILovePDF, message: Message):
     if (dataBASE.MONGODB_URI) and (message.chat.id not in GROUPS):
         await log.newUser(bot, message, False, False)
         GROUPS.append(message.chat.id)
+
     return True
 
 
 set_db = filters.create(setDb)
 @ILovePDF.on_message(filters.group & set_db & banned_group & filters.incoming)
-async def bannedGrp(bot, message):
+async def bannedGrp(bot: ILovePDF, message: Message):
     try:
         lang_code = await util.getLang(message.chat.id)
         await message.reply_chat_action(enums.ChatAction.TYPING)
+        
         if message.chat.id in BANNED_GRP_DB:
-            ban = await db.get_key(id=message.chat.id, key="banned", typ="group")
+            ban = await db.get_key(id = message.chat.id, key = "banned", typ = "group")
+
             trans_txt, trans_btn = await util.translate(
-                text="BAN['GroupCantUseDB']", button="BAN['banCB']", lang_code=lang_code
+                text = "BAN['GroupCantUseDB']", button = "BAN['banCB']", lang_code = lang_code
             )
+
             toPin = await message.reply_photo(
-                photo=images.BANNED_PIC,
-                reply_markup=trans_btn,
-                quote=True,
+                photo = images.BANNED_PIC,
+                reply_markup = trans_btn, quote = True,
                 caption=trans_txt.format(message.chat.title, ban),
             )
+
         else:
             trans_txt, trans_btn = await util.translate(
-                text="BAN['GroupCantUse']", button="BAN['banCB']", lang_code=lang_code
+                text = "BAN['GroupCantUse']", button = "BAN['banCB']", lang_code = lang_code
             )
+
             toPin = await message.reply_photo(
-                photo=images.BANNED_PIC,
-                reply_markup=trans_btn,
-                caption=trans_txt.format(message.chat.title),
-                quote=True,
+                photo = images.BANNED_PIC,
+                reply_markup = trans_btn,
+                caption = trans_txt.format(message.chat.title),
+                quote = True,
             )
+
         try:
             await toPin.pin()
         except Exception:
             pass
+
         await bot.leave_chat(message.chat.id)
+
     except Exception as Error:
         logger.exception("🐞 %s bannedGrp: %s" % (file_name, Error))
 
 
 #   IF FORCE SUBSCRIPTION
-async def notSubscribed(_, bot, message: Message):
+async def notSubscribed(_, bot: ILovePDF, message: Message):
+
     if message.text and message.text.startswith("/start"):
         msg = message.text.split(" ")
+
         if "-" in message.text:
             lang_code, referID, get_pdf, md5_str = await extract_data(
                 f"{message.text}-"
@@ -163,12 +247,14 @@ async def notSubscribed(_, bot, message: Message):
                 userLang[message.chat.id] = lang_code
         else:
             referID = None
-        lang_code = await util.getLang(message.chat.id)
+        
+        lang_code: str = await util.getLang(message.chat.id)
         if dataBASE.MONGODB_URI:  # CHECK IF USER IN DATABASE
             await log.newUser(bot, message, lang_code, referID)
 
     if len(invite_link) == 0:
         return False  # IF FORCE SUB. TREAT AS SUBSCRIBED
+    
     else:
         try:
             userStatus = await bot.get_chat_member(
@@ -177,8 +263,10 @@ async def notSubscribed(_, bot, message: Message):
             if userStatus.status == "kicked":  # IF USER BANNED FROM CHANNEL
                 return True
             return False
+        
         except UserNotParticipant:
             return True
+        
         except Exception as e:
             return True
 
@@ -205,18 +293,20 @@ async def non_subscriber(bot, message):
                 code = ""
         else:
             code = ""
+
         tTXT, tBTN = await util.translate(
-            text="BAN['Force']",
-            button="BAN['ForceCB']",
-            asString=True,
-            lang_code=lang_code,
+            text = "BAN['Force']",
+            button = "BAN['ForceCB']",
+            asString = True,
+            lang_code = lang_code,
         )
-        tBTN = await util.editDICT(inDir=tBTN, value=[invite_link[0], code])
+
+        tBTN = await util.editDICT(inDir = tBTN, value = [invite_link[0], code])
         await message.reply_photo(
-            photo=images.WELCOME_PIC,
-            quote=True,
-            caption=tTXT.format(message.from_user.first_name, message.from_user.id),
-            reply_markup=await util.createBUTTON(btn=tBTN, order="11"),
+            photo = images.WELCOME_PIC,
+            quote = True,
+            caption = tTXT.format(message.from_user.first_name, message.from_user.id),
+            reply_markup = await util.createBUTTON(btn=tBTN, order="11"),
         )
         if (
             settings.MULTI_LANG_SUP
@@ -225,10 +315,10 @@ async def non_subscriber(bot, message):
             and lang_code == "eng"
         ):
             change, close = await util.translate(
-                text="SETTINGS['lang']",
-                button="RESTART['btn']",
-                asString=True,
-                lang_code=lang_code,
+                text = "SETTINGS['lang']",
+                button = "RESTART['btn']",
+                asString = True,
+                lang_code = lang_code,
             )
             LANG = {
                 langList[lang][1]: f"https://t.me/{myID[0].username}?start=-l{lang}"
@@ -236,12 +326,13 @@ async def non_subscriber(bot, message):
             }
             LANG.update(close)
             BUTTON = await util.createBUTTON(
-                btn=LANG, order=int(f"{((len(LANG)-1)//2)*'2'}{(len(LANG)-1)%2}1")
+                btn = LANG, order = int(f"{((len(LANG)-1)//2)*'2'}{(len(LANG)-1)%2}1")
             )
-            await message.reply(text=change, reply_markup=BUTTON)
+            await message.reply(text = change, reply_markup = BUTTON)
         return
     except Exception as Error:
         logger.exception("🐞 %s non_subscriber: %s" % (file_name, Error))
+
 
 # If you have any questions or suggestions, please feel free to reach out.
 # Together, we can make this project even better, Happy coding!  XD
